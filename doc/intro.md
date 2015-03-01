2310421066f4e9732fd43a6c3d0ba8929470ceae
Dersu Uzala <attila@egri-nagy.hu>
2015-03-01 17:28:47 +1100

    generated lein product structure

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to kigen
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/kigen/core_test.clj
@@ -0,0 +1,7 @@
+(ns kigen.core-test
+  (:require [clojure.test :refer :all]
+            [kigen.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
