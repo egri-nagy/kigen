7a155b2dacfd9491ca00825ff4354e49d6ca74a9
Attila Egri-Nagy <attila@egri-nagy.hu>
2015-04-02 20:18:33 +1100

    added rand-subset

--- src/kigen/core.clj
+++ src/kigen/core.clj
@@ -2,11 +2,9 @@
   (:require [kigen.pbr :as pbr]) (:gen-class))
 
 (defn -main
-  "I don't do a whole lot ... yet."
+  "just a main"
   [& args]
-  (println "Hello, World!")
-  (println {1 #{1 2}, 2 #{2}} )
-  (println (pbr/cod-cod [] 0)))
+  (println (pbr/rand-pbr 3 4)))
 
 ; another way, with keywords
 (def kw  (keyword (str 12)))
--- src/kigen/pbr.clj
+++ src/kigen/pbr.clj
@@ -5,7 +5,17 @@
 ;; for degree n, domain is 1..n, codomain is n+1..2n
 ;; e.g. degree 3, domain is {1,2,3}, codomain is {4,5,6}
 
-(defn cod-cod
-  "the relation within the codomain only"
-  [pbr k]
-  nil)
+; generating a vector of booleans as a characteristic function
+(defn rand-subset
+  "returns a random subset of the given collection"
+  [coll]
+  (let [bits (take (count coll) (repeatedly (partial rand-nth [true false])))]
+    (set (map second (filter first (map list  bits coll))))))
+
+(defn rand-pbr
+  "a random (n,m) paritioned binary relation"
+  [m n]
+  (let [ pbr {:dom (set (range 1 (inc m)))
+              :cod (set (range (inc m) (+ m n 1)))}]
+    (reduce conj pbr (take 5 (repeat [1 #{1}])))
+    ))
