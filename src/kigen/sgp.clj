1ba38e11f794cc5340acf4e155027789793585e9
Attila Egri-Nagy <attila@egri-nagy.hu>
2015-04-14 20:00:29 +1000

    new namespace for semigroups

---
+++ src/kigen/sgp.clj
@@ -0,0 +1,8 @@
+(ns kigen.sgp)
+
+;; semigroup by generators TODO maybe a general orbit algorithm will do
+;; gens - generator elements
+;; leftmul - left multiplication
+(defn sgp-by-gens
+  [gens leftmul]
+)
