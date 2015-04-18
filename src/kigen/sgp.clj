2dd7b3f08d78035705ac40629597dbe564843bb3
Attila Egri-Nagy <attila@egri-nagy.hu>
2015-04-18 11:54:04 +1000

    working semigroup function, but needs to recur with the total set of elements as well

--- src/kigen/sgp.clj
+++ src/kigen/sgp.clj
@@ -1,4 +1,6 @@
-(ns kigen.sgp)
+(ns kigen.sgp
+  (:require [clojure.set :as set]))
+
 
 ;; semigroup by generators TODO maybe a general orbit algorithm will do
 ;; gens - generator elements
@@ -6,4 +8,9 @@
 (defn sgp-by-gens
   [gens rightmul]
   (let [funcs (for [x gens] #(rightmul % x))]
-    funcs))
+    (loop [orbit [(set gens)]]
+      (if (empty? (last orbit))
+        orbit
+        (recur (conj orbit (set/difference
+                            (set (for [x (last orbit) f funcs] (f x)))
+                            (reduce set/union #{} orbit))))))))
