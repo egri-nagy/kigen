6eecabf839a7f5663ece50ca59f9fb95b3270036
Attila Egri-Nagy <attila@egri-nagy.hu>
2015-04-18 12:16:55 +1000

    less double calculation now, but still not good enough

--- src/kigen/sgp.clj
+++ src/kigen/sgp.clj
@@ -1,16 +1,17 @@
 (ns kigen.sgp
   (:require [clojure.set :as set]))
 
-
 ;; semigroup by generators TODO maybe a general orbit algorithm will do
 ;; gens - generator elements
 ;; rightmul - right multiplication
 (defn sgp-by-gens
   [gens rightmul]
   (let [funcs (for [x gens] #(rightmul % x))]
-    (loop [orbit [(set gens)]]
-      (if (empty? (last orbit))
-        orbit
-        (recur (conj orbit (set/difference
-                            (set (for [x (last orbit) f funcs] (f x)))
-                            (reduce set/union #{} orbit))))))))
+    (loop [orbit [(set gens)]
+           total #{}]
+      (let [frontline (last orbit)
+            newelts (set (for [x frontline f funcs] (f x)))]
+        (if (empty? frontline)
+          (reduce into #{} orbit)
+          (recur (conj orbit (set/difference newelts total))
+                 (into total newelts)))))))
