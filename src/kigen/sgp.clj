480ea7e1c1205f5cd07946accf3af394e7e81d1b
Attila Egri-Nagy <attila@egri-nagy.hu>
2015-04-18 12:32:25 +1000

    commenting, and actually, there was still possibility for simplification

--- src/kigen/sgp.clj
+++ src/kigen/sgp.clj
@@ -3,13 +3,15 @@
 
 ;; semigroup by generators TODO maybe a general orbit algorithm will do
 ;; gens - generator elements
-;; rightmul - right multiplication
+;; mul - multiplication for generator elements
 (defn sgp-by-gens
-  [gens rightmul]
-  (let [funcs (for [x gens] #(rightmul % x))]
-    (loop [orbit [(set gens)] total #{}]
+  [gens mul]
+  (let [funcs (for [x gens] #(mul % x))] ;right multiplication functions
+    ;; orbit - vector of sets containing semigroup elements in production order
+    ;; total - cumulative union of orbit elements
+    (loop [orbit [(set gens)] total (first orbit)]
       (let [newelts (set (for [x (last orbit) f funcs] (f x)))
             diff (set/difference newelts total)]
         (if (empty? diff)
-          (reduce into #{} orbit)
+          total
           (recur (conj orbit diff) (into total diff)))))))
