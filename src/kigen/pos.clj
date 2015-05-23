1c2a22acc37000dcaa6c27bc19383e26933bdf12
Attila Egri-Nagy <attila@egri-nagy.hu>
2015-05-23 16:45:43 +1000

    adding pos

---
+++ src/kigen/pos.clj
@@ -0,0 +1,16 @@
+(ns kigen.pos)
+
+;; Finding positions of elements in a collection.
+;; adapted from The Joy of Clojure, Second Edition,
+;; Manning Publications 2014, pp111-113
+
+(defn index [coll]
+  (cond (set? coll) (map vector coll coll) ;elements in set index themselves
+        (map? coll) (seq coll) ;seq-ing a map gives key value pairs in vectors
+        :else (map vector (iterate inc 0) coll))) ;list, vectors index from 0
+
+(defn positions [pred coll]
+  (for [[i v] (index coll) :when (pred v)] i))
+
+(defn pos [pred coll]
+  (first (positions pred coll)))
