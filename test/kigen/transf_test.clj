da7d345ee381dd0670e33a9e05c449cf0588e856
Attila Egri-Nagy <attila@egri-nagy.hu>
2015-06-01 13:59:10 +1000

    added test to the image list conversion

--- test/kigen/transf_test.clj
+++ test/kigen/transf_test.clj
@@ -1,12 +1,20 @@
 (ns kigen.transf-test
   (:require [clojure.test :refer :all]
-            [kigen.core :refer :all]))
+            [kigen.transf :as transf]
+            [kigen.sgp :as sgp]
+            [kigen.pbr :as pbr]))
 
 (deftest pbr-full-ts-test
   (testing "Testing full transformation semigroups."
     (let [pow #(reduce * 1 (repeat %2 %1))]
       (doseq [n [1 2 3 4 5]]
         (is (= (pow n n)
-               (count(kigen.sgp/sgp-by-gens
-                      (kigen.transf/full-ts-gens n)
-                      kigen.pbr/mul))))))))
+               (count(sgp/sgp-by-gens (transf/full-ts-gens n) pbr/mul))))))))
+
+(deftest pbr-transf-test
+  (testing "Testing converting to image list and back."
+    (let [pred #(= (transf/transf->pbr (transf/pbr->transf %)) %)]
+      (doseq [n [1 2 3 4]]
+        (is (= true
+               (every? pred
+                       (sgp/sgp-by-gens (transf/full-ts-gens n) pbr/mul))))))))
