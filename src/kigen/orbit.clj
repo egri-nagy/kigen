8dcabb34581695f261e4766cc1c69966ab250a52
Attila Egri-Nagy <attila@egri-nagy.hu>
2015-06-02 14:13:59 +1000

    adapting an excellent Tarjan's scc implementation from clj-me

--- src/kigen/orbit.clj
+++ src/kigen/orbit.clj
@@ -59,10 +59,12 @@
                     (let [[[k v]] (seq (og e))]
                       (recur v (conj r k))))))))
 
+;; stack: node and operation pairs, the work that still needs to be done
 (defn dfs
   [start ops]
-  (let [load #(for [op ops] [% op])]
-    (loop [stack (into [] (load start))  coll #{}]
+  (let [opf #(for [op ops] [% op])] ;generating node-op pairs to be pushed
+    (loop [stack (into [] (opf start))
+           coll #{}]
       (if (empty? stack)
         coll
         (let [[e op] (peek stack)
@@ -70,4 +72,43 @@
               nstack (pop stack)]
           (if (contains? coll ne)
             (recur nstack coll)
-            (recur (into nstack (load ne)) (conj coll ne))))))))
+            (recur (into nstack (opf ne)) (conj coll ne))))))))
+
+(defn digraph
+  [nodes ops]
+  (into {} (for [n nodes]
+           [n (vec (for [o ops] (o n)))])))
+
+;;clj-me.cgrand.net/2013/03/18/tarjans-strongly-connected-components-algorithm/
+;;en.wikipedia.org/wiki/Tarjan's_strongly_connected_components_algorithm
+
+;;env is a map from nodes to stack length or nil,
+;;nil means the node is known to belong to another SCC
+;; ::stack  the current stack
+;; ::sccs   the current set of SCCs
+(defn scc
+  "Returns the strongly connected components of a graph specified by its nodes
+  and a successor function succs from node to nodes."
+  [nodes succs]
+  (letfn [(strongconn [env node]
+            (if (contains? env node)
+              env ;nothing to do, the node is already visited
+              (let [stack (::stack env)
+                    n (count stack)
+                    env (assoc env node n ::stack (conj stack node))
+                    env (reduce (fn [env succ]
+                                  (let [env (strongconn env succ)]
+                                    (assoc env
+                                           node
+                                           (min (or (env succ) n) (env node)))))
+                                env (succs node))]
+                (if (= n (env node)) ; no link below, an SCC found
+                  (let [nodes (::stack env)
+                        scc (set (take (- (count nodes) n) nodes))
+                        env (reduce #(assoc %1 %2 nil) env scc)] ;clear lengths
+                    (assoc env ::stack stack ::sccs (conj (::sccs env) scc)))
+                  env))))]
+    (::sccs ;getting the result from the environment
+     (reduce strongconn
+             {::stack () ::sccs #{}} ;initial environment
+             nodes))))
