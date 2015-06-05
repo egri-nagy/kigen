1aab2712b456fa90546c5e5dfee2c9a496a1dab3
Attila Egri-Nagy <attila@egri-nagy.hu>
2015-06-05 15:41:31 +1000

    adding the holonomy file

---
+++ src/kigen/holonomy.clj
@@ -0,0 +1,14 @@
+(ns kigen.holonomy
+  (:use [kigen.orbit :as o]))
+
+;; elts - set of elements
+;; rel? - a partial order relation pred on elts
+(defn hasse-diagram
+  [elts rel?]
+  )
+
+;; set
+;; hd Hasse-diagram
+(defn tile-chain
+  [set hd]
+  )
