dc1f79e640d8af9d47a7a87a6d5b5bada967683e
Attila Egri-Nagy <attila@egri-nagy.hu>
2015-06-05 20:37:22 +1000

    hasse-diag half-way done, but getting sleepy

--- src/kigen/holonomy.clj
+++ src/kigen/holonomy.clj
@@ -2,10 +2,14 @@
   (:use [kigen.orbit :as o]))
 
 ;; elts - set of elements
-;; rel? - a partial order relation pred on elts
+;; rel - a partial order relation pred on elts
 (defn hasse-diagram
-  [elts rel?]
-  )
+  [elts rel]
+  (letfn [(insert [hd key newval]
+            (assoc hd key (conj (hd key) newval)))
+          (g [hd e]
+            (reduce #(insert % %2 e) hd  (filter #(rel e %) (keys hd))))]
+    (reduce g (into {} (for [e elts] [e #{}])) elts)))
 
 ;; set
 ;; hd Hasse-diagram
