9f2d20e2371884c293284c29b31f40d0e437eb1e
Attila Egri-Nagy <attila@egri-nagy.hu>
2015-06-08 19:24:55 +1000

    equivalence added

--- src/kigen/holonomy.clj
+++ src/kigen/holonomy.clj
@@ -6,3 +6,8 @@
   [P Q gens action]
   (or (set/subset? P Q)
       (contains? (o/orbit Q gens action) P)))
+
+(defn equivalent?
+  [P Q gens action]
+  (and (subduction P Q gens action)
+       (subduction Q P gens action)))
