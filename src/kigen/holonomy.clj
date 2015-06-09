9f75a5dc13cedf8693ee5a7dc0d6bc0c6189ea44
Dersu Uzala <attila@egri-nagy.hu>
2015-06-09 14:34:34 +1000

    for holonomy we don't need action, as it is transformation action always

--- src/kigen/holonomy.clj
+++ src/kigen/holonomy.clj
@@ -1,17 +1,18 @@
 (ns kigen.holonomy
   (:use [kigen.orbit :as o]
-        [clojure.set :as set]))
+        [clojure.set :as set]
+        [kigen.transf :as t]))
 
 (defn finite-set
   [n]
   (set (range 1 (inc n))))
 
 (defn subduction
-  [P Q gens action]
+  [P Q gens]
   (or (set/subset? P Q)
-      (contains? (o/orbit Q gens action) P)))
+      (contains? (o/orbit Q gens t/act) P)))
 
 (defn equivalent?
-  [P Q gens action]
-  (and (subduction P Q gens action)
-       (subduction Q P gens action)))
+  [P Q gens]
+  (and (subduction P Q gens t/act)
+       (subduction Q P gens t/act)))
