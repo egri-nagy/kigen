79a17a0bcda38972d2d22a3b58bc957a4539538a
Attila Egri-Nagy <attila@egri-nagy.hu>
2015-06-14 13:55:49 +1000

    calculating heights finished

--- src/kigen/holonomy.clj
+++ src/kigen/holonomy.clj
@@ -31,6 +31,16 @@
     (some #(subduction? (first %) (second %) gens)
           (for [P clA Q clB] [P Q]))))
 
+(defn calc-heights
+  [eqvcls gens]
+  (let [nonsingl-eqvcls (remove #(t/singleton? (first %)) eqvcls)
+        sur-hd (p/hasse-diagram nonsingl-eqvcls (class-surduction gens))
+        minimals (filter #(empty? (sur-hd %)) nonsingl-eqvcls)
+        sub-hd (p/hasse-diagram nonsingl-eqvcls (class-subduction gens))
+        height-tabs (map #(p/max-distances-from % sub-hd) minimals)]
+    (map (fn [k] [k (inc (apply max (map #(% k) height-tabs)))])
+         (keys sur-hd))))
+
 ;; creates a big map of holding all the skeleton information
 (defn skeleton
   [gens]
@@ -38,11 +48,9 @@
         images (o/orbit stateset gens t/act)
         c-g (o/cayley-graph images (for [x gens] #(t/act % x)))
         sccs (o/scc images c-g)
-        nonsingleton-sccs (remove #(t/singleton? (first %)) sccs)
-        surduction-hd (p/hasse-diagram nonsingleton-sccs class-surduction)
-        minimals (filter #()  nonsingleton-sccs)
-        heights ()]
+        heights (calc-heights sccs gens)]
     {:stateset stateset
      :images images
      :equivclasses sccs
+     :heights heights
      }))
