c1daec33955fec9121c04a22c91a40e65bf259ad
Attila Egri-Nagy <attila@egri-nagy.hu>
2015-06-29 21:31:41 +1000

    yikes! tests run fine

--- src/kigen/orbit.clj
+++ src/kigen/orbit.clj
@@ -17,20 +17,17 @@
 ;; funcs-seq - sequence of function colls
 ;; in each step we may apply different set of functions
 (defn alternating-orbit
-  [seed funcs-seq]
+  [seeds fs]
   ;; o - vector of sets containing orbit elements in production order
   ;; total - cumulative union of orbit element
-  (loop [o [(set  seed)]
-         total (first o)
-         funcs-seq funcs-seq]
-    (let [newelts  (for [x (last o) f (first funcs-seq)] (f x))
-          newset (reduce into #{} newelts)
+  (loop [o [(set  seeds)]
+         total (first o)]
+    (let [newset (set (for [x (last o) f fs] (f x)))
           diff (difference newset total)]
       (if (empty? diff)
         total
         (recur (conj o diff)
-               (into total diff)
-               (rest funcs-seq))))))
+               (into total diff))))))
 
 
 (defn actions
--- src/kigen/sgp.clj
+++ src/kigen/sgp.clj
@@ -1,10 +1,9 @@
 (ns kigen.sgp
-  (:use [kigen.orbit :only [orbit]]))
+  (:use [kigen.orbit :only [alternating-orbit actions]]))
 
 ;; semigroup by generators
 ;; gens - generator elements
 ;; mul - multiplication for generator elements
 (defn sgp-by-gens
   [gens mul]
-  (orbit gens ;generators are already known elements
-         [#(for [x gens] (mul % x))])) ;right multiplication functions
+  (alternating-orbit gens (actions gens mul))) 
