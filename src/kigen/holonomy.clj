7f6c9d656476c876452e4174fbb1d6bb9b2c814e
Attila Egri-Nagy <attila@egri-nagy.hu>
2015-07-31 10:49:26 +1000

    hasse-diagram -> cover-rel

--- src/kigen/holonomy.clj
+++ src/kigen/holonomy.clj
@@ -44,8 +44,8 @@
 (defn calc-heights
   [eqvcls gens]
   (let [nonsingl-eqvcls (remove #(t/singleton? (first %)) eqvcls)
-        sur-hd (p/hasse-diagram nonsingl-eqvcls (class-surduction gens))
-        sub-hd (p/hasse-diagram nonsingl-eqvcls (class-subduction gens))
+        sur-hd (p/cover-rel nonsingl-eqvcls (class-surduction gens))
+        sub-hd (p/cover-rel nonsingl-eqvcls (class-subduction gens))
         minimals (filter #(empty? (sur-hd %)) nonsingl-eqvcls)
         height-tabs (map #(p/max-distances % sub-hd) minimals)]
     (into {} (map (fn [k] [k (inc (apply max (remove nil? (map
@@ -81,8 +81,8 @@
      :equivclasses sccs
      :heights heights
      :height (heights stateset)
-     :subsethd (p/hasse-diagram extd set/subset?)
-     :supsethd (p/hasse-diagram extd set/superset?)}))
+     :subsethd (p/cover-rel extd set/subset?)
+     :supsethd (p/cover-rel extd set/superset?)}))
 
 (defn depth [sk P] (inc (- (:height sk) ((:heights sk) P))))
 
--- src/kigen/poset.clj
+++ src/kigen/poset.clj
@@ -2,7 +2,7 @@
 (ns kigen.poset
   (:use [clojure.set :only [union]]))
 
-(declare hasse-diagram ;calculates the Hasse-diagram graph of a relation
+(declare cover-rel ;calculates the covering relation of a relation
          chain-extensions ;extends chain by one more element
          all-chains ;all chains from an element
          max-distances) ;maximal distances from an element
@@ -10,21 +10,21 @@
 ;; Not the most clever algorithm: it assumes that anything related is a cover
 ;; then gets rid of it if proven not to be a cover
 ;; elts - set of elements
-;; rel - a partial order relation predicate
-(defn hasse-diagram
-  "Creates the Hasse-diagram of a partial order as a graph
+;; rel? - a partial order relation predicate
+(defn cover-rel
+  "The covering relation of a partial order as a graph
   (map: element -> set of covers), given by the elements and a predicate."
-  [elts rel]
+  [elts rel?]
   (letfn [(recalc-covers [covers newval] ;recalculate the set of covers
-            (if (some #(rel newval %) covers) ;nothing to do newval is below
+            (if (some #(rel? newval %) covers) ;nothing to do newval is below
               covers
-              (conj (set (filter #(not (rel % newval)) covers)) newval)))
-          (insert [hd e] ;insert an element into the graph by updating covers
+              (conj (set (filter #(not (rel? % newval)) covers)) newval)))
+          (insert [cr e] ;insert an element into the graph by updating covers
             (let [candidates (filter #(and (not (= e %))
-                                           (rel e %))
-                                     (keys hd))]
+                                           (rel? e %))
+                                     (keys cr))]
               (reduce #(assoc % %2 (recalc-covers (%  %2) e))
-                      hd
+                      cr
                       candidates)))]
     ;starting with the empty covering sets, we insert all elements
     (reduce insert
@@ -32,18 +32,18 @@
             elts)))
 
 (defn chain-extensions
-  "Returns all chains extending chain by one more element in Hasse-diagram hd."
-  [chain hd]
-  (map (partial conj chain) (hd (last chain))))
+  "Returns all chains extending chain by one more element in cover-rel cr."
+  [chain cr]
+  (map (partial conj chain) (cr (last chain))))
 
 (defn all-chains
-  "All chains from element e in the Hasse-diagram hd."
-  [e hd]
+  "All chains from element e in the cover-rel cr."
+  [e cr]
   (loop [stack [[e]] chains #{}]
     (if (empty? stack)
       chains
       (let [chain (peek stack)
-            exts (chain-extensions chain hd)]
+            exts (chain-extensions chain cr)]
         (if (empty? exts)
           (recur (pop stack) (conj chains chain))
           (recur (into (pop stack) exts) chains))))))
@@ -51,7 +51,7 @@
 (defn max-distances
   "Returns a map of elements to their maximal distance (length of longest chain)
   from element e."
-  [e hd]
+  [e cr]
   (loop [d 0
          dists {}
          frontier #{e}]
@@ -59,4 +59,4 @@
       dists
       (recur (inc d)
              (reduce #(assoc % %2 d) dists frontier)
-             (apply union (map hd frontier))))))
+             (apply union (map cr frontier))))))
