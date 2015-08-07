da347bc248e4658796fd002dece05fbde9e427dd
Attila Egri-Nagy <attila@egri-nagy.hu>
2015-08-07 14:35:07 +1000

    got it working, but needs clarification

--- src/kigen/multab.clj
+++ src/kigen/multab.clj
@@ -1,10 +1,13 @@
 (ns kigen.multab
-  (:use [kigen.orbit :as o]
-        [kigen.transf :as t]
-        [kigen.poset :as p]
-        [clojure.set :as set]))
+  (:use [kigen.pos :as pos]))
 
 (defn multab
   "Returns the multiplication table of the elements xs by the function mul."
   [xs mul]
-  (vec (pmap #(vec (map (partial mul %) xs)) xs)))
+  (let [indices (pos/index (vec xs))]
+    (map
+     (fn [x]
+       (map
+        #(pos/pos (partial = %) xs indices)
+        (map #(mul x %) xs)))
+     xs)))
