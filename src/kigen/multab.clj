3405eb26f83eeaf92bf44736b1607a605fa91446
Attila Egri-Nagy <attila@egri-nagy.hu>
2015-08-16 16:38:37 +1000

    first draft of the closure

--- src/kigen/multab.clj
+++ src/kigen/multab.clj
@@ -1,5 +1,6 @@
 (ns kigen.multab
-  (:use [kigen.pos :as pos]))
+  (:use [clojure.set :only [difference union]]
+        [kigen.pos :as pos]))
 
 (defn multab
   "Returns the multiplication table of the elements xs by the function mul."
@@ -17,7 +18,18 @@
   [mt elts]
   (set (for [i elts j elts] (nth (nth mt i) j))))
 
+(defn extend-by
+  [mt base exts]
+  (let [u (union base exts)]
+    (union (set (for [i exts j u] (nth (nth mt i) j)))
+           (set (for [i base j exts] (nth (nth mt i) j))))))
+
 (defn closure
   "Returns the smallest closed subarray that contains the elements."
-  [mt elts]
-  )
+  [mt elts] (loop [base (union (set elts) (content mt elts))
+                   exts (difference base elts)]
+              (if (empty? exts)
+                base
+                (let [nbase (union base exts (extend-by mt base exts))
+                      nexts (difference nbase (union base exts) )]
+                  (recur nbase nexts)))))
