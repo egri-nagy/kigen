aec31fe385d9d7ac84b912b0339fcf14a0508519
Dersu Uzala <attila@egri-nagy.hu>
2015-09-05 19:06:51 +1000

    oops, the orbit test was half-disabled

--- test/kigen/orbit_test.clj
+++ test/kigen/orbit_test.clj
@@ -8,15 +8,9 @@
 (defn subset-covers [coll]
   (map #(set (remove (partial = %) coll)) coll))
 
-(deftest test-sdfs
-  (testing "Testing depth-first search with set-valued operator."
-    (is (= 32 (count (orbit/dfs [#{1 2 3 4 5}] subset-covers))))))
-
-(comment  (deftest test-*fs
-            (let [ops (for [x [1 2 3 4 5]] #(set (remove (partial = x ) %)))
-                  bfsres (orbit/bfs [#{1 2 3 4 5}] ops)
-                  dfsres (orbit/dfs [#{1 2 3 4 5}] ops)
-                  dfs2res (orbit/dfs2 [#{1 2 3 4 5}] ops)]
-              (testing "Testing depth-first and breadth-first searches."
-                (is (= 32 (count bfsres)))
-                (is (= bfsres dfsres dfs2res))))))
+(deftest test-*fs
+  (let [bfsres (orbit/bfs [#{1 2 3 4 5 6 7 8}] subset-covers)
+        dfsres (orbit/dfs [#{1 2 3 4 5 6 7 8}] subset-covers)]
+    (testing "Testing depth-first and breadth-first searches."
+      (is (= 256 (count bfsres)))
+      (is (= bfsres dfsres)))))
