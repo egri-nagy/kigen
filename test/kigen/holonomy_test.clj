07085bdeecb903272e0b002f0706f528aacce031
Dersu Uzala <attila@egri-nagy.hu>
2015-09-06 08:21:59 +1000

    just adding the file since movoving to other machine

---
+++ test/kigen/holonomy_test.clj
@@ -0,0 +1,22 @@
+(ns kigen.orbit-test
+  (:require [clojure.test :refer :all]
+            [kigen.orbit :as orbit]
+            [kigen.pbr :as pbr]))
+
+;;testing with operators that construct subsets of a set
+;;a set-valued operator
+(defn subset-covers [coll]
+  (map #(set (remove (partial = %) coll)) coll))
+
+(deftest test-sdfs
+  (testing "Testing depth-first search with set-valued operator."
+    (is (= 32 (count (orbit/dfs [#{1 2 3 4 5}] subset-covers))))))
+
+(comment  (deftest test-*fs
+            (let [ops (for [x [1 2 3 4 5]] #(set (remove (partial = x ) %)))
+                  bfsres (orbit/bfs [#{1 2 3 4 5}] ops)
+                  dfsres (orbit/dfs [#{1 2 3 4 5}] ops)
+                  dfs2res (orbit/dfs2 [#{1 2 3 4 5}] ops)]
+              (testing "Testing depth-first and breadth-first searches."
+                (is (= 32 (count bfsres)))
+                (is (= bfsres dfsres dfs2res))))))
