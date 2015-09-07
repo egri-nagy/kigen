4b570e197034297030a1b597b76c13293b270b74
Attila Egri-Nagy <attila@egri-nagy.hu>
2015-09-07 20:28:05 +1000

    writing proper test

--- test/kigen/holonomy_test.clj
+++ test/kigen/holonomy_test.clj
@@ -1,22 +1,14 @@
-(ns kigen.orbit-test
+(ns kigen.holonomy-test
   (:require [clojure.test :refer :all]
-            [kigen.orbit :as orbit]
+            [kigen.holonomy :as h]
+            [kigen.sgp :as sgp]
+            [kigen.transf :as transf]
             [kigen.pbr :as pbr]))
 
-;;testing with operators that construct subsets of a set
-;;a set-valued operator
-(defn subset-covers [coll]
-  (map #(set (remove (partial = %) coll)) coll))
+(load-file "resources/sgpbestiary.clj")
 
-(deftest test-sdfs
-  (testing "Testing depth-first search with set-valued operator."
-    (is (= 32 (count (orbit/dfs [#{1 2 3 4 5}] subset-covers))))))
+(def BECKS (sgp/sgp-by-gens (map transf/transf->pbr becks) pbr/mul))
 
-(comment  (deftest test-*fs
-            (let [ops (for [x [1 2 3 4 5]] #(set (remove (partial = x ) %)))
-                  bfsres (orbit/bfs [#{1 2 3 4 5}] ops)
-                  dfsres (orbit/dfs [#{1 2 3 4 5}] ops)
-                  dfs2res (orbit/dfs2 [#{1 2 3 4 5}] ops)]
-              (testing "Testing depth-first and breadth-first searches."
-                (is (= 32 (count bfsres)))
-                (is (= bfsres dfsres dfs2res))))))
+(deftest test-holonomy
+  (testing "Testing holonomy decomposition."
+    (is (= 5 (:height (h/skeleton BECKS))))))
