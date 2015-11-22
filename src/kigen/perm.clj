110358b70c0de455c6f48f0dd0ce2a89f628ee5b
Attila Egri-Nagy <attila@egri-nagy.hu>
2015-11-22 20:25:48 +1100

    adding conjugation

--- src/kigen/perm.clj
+++ src/kigen/perm.clj
@@ -1,5 +1,6 @@
 ;;permutations
 (ns kigen.perm
+  (:require [kigen.pbr :as pbr])
   (:use [kigen.transf :only [binrel-transf?]]))
 
 ;;pbr is a permutation if images of elements of the domain yield the codomain
@@ -7,3 +8,8 @@
   [pbr]
   (and (binrel-transf? pbr)
        (= (:cod pbr) (reduce into #{} (for [key (:dom pbr)] (pbr key))))))
+
+(defn conjugate
+  "the conjugate of x by p, i.e. p^{-1}xp"
+  [x p]
+  (pbr/mul (pbr/mul (pbr/flip p) x) p))
