aa53aada3e398bf04117163e99f16845ae084588
Attila Egri-Nagy <attila@egri-nagy.hu>
2015-12-09 14:50:55 +1100

    sgp-by-gens can now default to pbr multiplication

--- src/kigen/sgp.clj
+++ src/kigen/sgp.clj
@@ -1,9 +1,11 @@
 (ns kigen.sgp
-  (:use [kigen.orbit :only [bfs actions action-function]]))
+  (:use [kigen.orbit :only [bfs actions action-function]])
+  (:require [kigen.pbr :as pbr]))
 
 ;; semigroup by generators
 ;; gens - generator elements
 ;; mul - multiplication for generator elements
 (defn sgp-by-gens
-  [gens mul]
-  (bfs gens (action-function (actions gens mul))))
+  ([gens] (sgp-by-gens gens pbr/mul))
+  ([gens mul]
+   (bfs gens (action-function (actions gens mul)))))
