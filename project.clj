1313b4a660a36e9d29a83e69ae96b4dc5616078a
Attila Egri-Nagy <attila@egri-nagy.hu>
2015-12-14 20:45:32 +1100

    commutative? added

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "https://github.com/egri-nagy/kigen"
   :license {:name "GNU GENERAL PUBLIC LICENSE"
             :url "http://www.gnu.org/copyleft/gpl.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/math.combinatorics "0.1.1"]]
   :main ^:skip-aot kigen.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/kigen/sgp.clj
+++ src/kigen/sgp.clj
@@ -1,6 +1,7 @@
 (ns kigen.sgp
   (:use [kigen.orbit :only [bfs actions action-function]])
-  (:require [kigen.pbr :as pbr]))
+  (:require [kigen.pbr :as pbr])
+  (:require [clojure.math.combinatorics :as combinatorics]))
 
 ;; semigroup by generators
 ;; gens - generator elements
@@ -9,3 +10,7 @@
   ([gens] (sgp-by-gens gens pbr/mul))
   ([gens mul]
    (bfs gens (action-function (actions gens mul)))))
+
+(defn commutative? [sgp]
+  (every? #(= (pbr/mul (first %) (second %)) (pbr/mul (second %) (first %)))
+          (combinatorics/combinations sgp 2)))
