b592cd6397a73a930c182e645456359f7d4a5f1a
Attila Egri-Nagy <attila@egri-nagy.hu>
2016-08-19 12:01:20 +0900

    better formulated subduction?

--- src/kigen/holonomy.clj
+++ src/kigen/holonomy.clj
@@ -12,11 +12,10 @@
 (defn subduction?
   [P Q afs]
   (or (set/subset? P Q)
-      (contains? (:solutions (o/controlled-dfs Q
+      (not-empty (:solutions (o/controlled-dfs Q
                                                afs
                                                #(<= (count P) (count %))
-                                               #(set/subset? % P)))
-                 P)))
+                                               #(set/superset? % P))))))
 
 ;;TODO extract the pattern for any pre-order
 (defn equivalent?
