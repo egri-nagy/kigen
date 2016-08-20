c61c6671394f818095a6ade3c2ce71e6822dee2c
Attila Egri-Nagy <attila@egri-nagy.hu>
2016-08-20 21:35:19 +0900

    oops, not even needed

--- src/kigen/holonomy.clj
+++ src/kigen/holonomy.clj
@@ -68,7 +68,7 @@
      :equivclasses sccs
      :heights heights
      :height (heights stateset)
-     :subsethd (p/cover-rel extd set/superset?)
+     ;:subsethd (p/cover-rel extd set/superset?)
      :supsethd (p/cover-rel extd set/subset?)}))
 
 (defn depth
