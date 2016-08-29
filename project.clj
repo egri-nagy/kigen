60f93725706c53cce67284970b0367bade1e5d7f
Attila Egri-Nagy <attila@egri-nagy.hu>
2016-08-29 14:38:29 +0900

    adding slamhound

--- project.clj
+++ project.clj
@@ -4,11 +4,14 @@
   :license {:name "GNU GENERAL PUBLIC LICENSE"
             :url "http://www.gnu.org/copyleft/gpl.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/math.combinatorics "0.1.3"]]
+                 [org.clojure/math.combinatorics "0.1.3"]
+                 [slamhound "1.5.5"]]
   :plugins [[lein-cloverage "1.0.6"]
             [lein-kibit "0.1.2"]
             [lein-ancient "0.6.10"]
+            [lein-bikeshed "0.3.0"]
             [jonase/eastwood "0.2.3"]]
   :main ^:skip-aot kigen.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all}}
+  :aliases {"slamhound" ["run" "-m" "slam.hound"]})
