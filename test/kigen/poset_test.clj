6e3b3055f43915bd226330f68302cb7a5c19f918
Attila Egri-Nagy <attila@egri-nagy.hu>
2016-09-12 09:50:17 +0900

    test separated as well

--- test/kigen/poset_test.clj
+++ test/kigen/poset_test.clj
@@ -5,19 +5,8 @@
 (deftest poset-explicit-test
   (testing "Testing implicit to explicit conversion."
     (let [r? (fn [x y] (= 0 (mod y x)))
-          r (poset/rel [1 2 4 8 16] r? )
+          r (poset/rel [1 2 4 8 16] r?)
           c (poset/cover-rel [1 2 4 8 16] r?)]
       (is (= r {1 #{1 2 4 8 16}, 2 #{2 4 8 16}, 4 #{4 8 16}, 8 #{8 16},
                 16 #{16}}))
-      (is (= c {1 #{2}, 2 #{4}, 4 #{8}, 8 #{16}, 16 #{}} )))))
-
-(deftest poset-chain-test
-  (testing "Testing chains in cover relation."
-    (let [r {1 #{2 3 4}, 2 #{6}, 3 #{5}, 4 #{5}, 5 #{6}}]
-      (is (=
-           (set (poset/chains 1 5 r))
-           #{[1 3 5], [1 4 5]} ))
-      (is (= [[1 2]] (poset/chains 1 2 r)))
-      (is (= 3 (count (poset/chains 1 6 r))))
-      (is (zero? (count (poset/chains 3 4 r))))
-      (is (= 1 (count (poset/chains 3 5 r)))))))
+      (is (= c {1 #{2}, 2 #{4}, 4 #{8}, 8 #{16}, 16 #{}})))))
