8ddc483478c2da0e5846563ce313c222d0f0ad87
Dersu Uzala <attila@egri-nagy.hu>
2016-10-28 22:04:45 +0900

    another namespace docstring

--- src/kigen/pbr.clj
+++ src/kigen/pbr.clj
@@ -1,11 +1,10 @@
 (ns kigen.pbr
+  "partitioned binary relations stored as maps: integers -> set of integers
+   e.g. {1 #{1 2}, 2 #{2}}
+   for degree n, domain is 1..n, codomain is n+1..2n
+   e.g. degree 3, domain is {1,2,3}, codomain is {4,5,6}"
   (:require [clojure.set :refer [union]]
             [kigen.orbit :refer [dfs]]))
-
-;; partitioned binary relations stored as maps: integers -> set of integers
-;; e.g. {1 #{1 2}, 2 #{2}}
-;; for degree n, domain is 1..n, codomain is n+1..2n
-;; e.g. degree 3, domain is {1,2,3}, codomain is {4,5,6}
 
 ;; RANDOM PARTITIONED BINARY RELATION ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (declare rand-pbr
