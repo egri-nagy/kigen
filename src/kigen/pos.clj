b204aa2b70cc01b18258c3d2cbf5e537bf990dce
Attila Egri-Nagy <attila@egri-nagy.hu>
2016-12-24 22:57:57 +0900

    adjusting docstrings

--- src/kigen/pos.clj
+++ src/kigen/pos.clj
@@ -1,13 +1,16 @@
 (ns kigen.pos
-  "Assigning position numbers to elements in a sequential collection.")
+  "Getting the indices of elements in vectors.")
 
 (defn positions
-  "All positions of elements satisfying predicate in a vector."
+  "All positions of elements of a vector satisfying a predicate."
   [pred v]
   (for [[index element] (map vector (range) v)
         :when (pred element)]
     index))
 
 (defn pos
-  "Getting the index of the first element satisfying a predicate."
-  [pred indxd] (first (positions pred indxd)))
+  "Getting the index of the first element of a vector satisfying a predicate."
+  [pred v]
+  (first (positions pred v)))
+
+;TODO SPEC
