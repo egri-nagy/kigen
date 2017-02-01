47a4da3395bec81dfc0dc5551a562313c3ca425f
Attila Egri-Nagy <attila@egri-nagy.hu>
2017-02-01 15:07:14 +0900

    putting together the backtrack, playing with the conditions

--- src/kigen/morphism.clj
+++ src/kigen/morphism.clj
@@ -17,15 +17,14 @@
   ([S T hom, dom, cod]
    (letfn [(f [x y] (let [z (at S x y)
                           t (at T (hom x) (hom y))]
-                      (or (and (contains? cod t) ; verbose logic, should be reduced
-                               (contains? dom z)
-                               (= t (hom z)))
-                          (and (not (contains? cod t))
-                               (not (contains? dom z))))))]
-     (nil? (first (for [x dom
-                        y dom
-                        :when (not (f x y))]
-                    [x y]))))))
+                      (not  (and (contains? cod t) ; experimenting with the condition, this one is too permissive
+                                 (contains? dom z)
+                                 (not= t (hom z))))
+                      ))]
+     (first (for [x dom
+                  y dom
+                  :when (not (f x y))]
+              [x y])))))
 
 (defn backtrack
   "S source set of elements, T target set of elements,
@@ -34,7 +33,6 @@
   choices - the possible next elements based on partial solution and source"
   [S,T,hom,  morphic?, choices]
   (if (= (count S) (count hom))
-    hom ; it is a solution
-    (let [morphicones (filter #(morphic? hom %) (choices hom S))
-          ]
-      nil)))
+    (print hom)
+    (map #(backtrack S T % morphic? choices)
+         (filter #(nil? (morphic? S T %)) (map #(conj hom %) choices)))))
