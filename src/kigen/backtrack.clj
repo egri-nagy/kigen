978e6ed64bdc1eebdc9ba7552046beecae0cb4d7
Attila Egri-Nagy <attila@egri-nagy.hu>
2017-02-01 11:26:28 +0900

    thinking about checking the homomorphism

--- src/kigen/backtrack.clj
+++ src/kigen/backtrack.clj
@@ -3,11 +3,32 @@
   input: two multiplication tables (source, target)
   output: vectors describing morphisms")
 
+;just for experimenting with
+(def T2 [ [ 0, 0, 3, 3 ],
+         [ 0, 1, 2, 3 ],
+         [ 0, 2, 1, 3 ],
+         [ 0, 3, 0, 3 ] ])
+
+(defmacro at [mt i j]
+    `((~mt ~i) ~j))
+
+(defn good? [S, T, hom, t]
+  (let [dom (range (count S))]
+    (every?
+     (fn [x] (every?
+              (fn [y] (= (hom (at S x y))
+                         (at T (hom x) (hom y))))
+              dom))
+     dom)))
+
 (defn backtrack
   "S source set of elements, T target set of elements,
+  psol - partial solution
   good? - takes a partial solution and the next element
-  choices - function returning the possible next elements based on partial solution and source" 
-  [S,T, good?, choices]
-
-  )
-
+  choices - the possible next elements based on partial solution and source"
+  [S,T,psol,  good?, choices]
+  (if (= (count S) (count psol))
+    psol ; it is a solution
+    (let [goodones (filter #(good? psol %) (choices psol S))
+          ]
+      nil)))
