a26603d506152c596b70ded55ca7d92f61761398
Attila Egri-Nagy <attila@egri-nagy.hu>
2017-02-02 23:03:41 +0900

    not to type kigen. all the time

--- src/kigen/core.clj
+++ src/kigen/core.clj
@@ -1,13 +1,14 @@
 (ns kigen.core
-  (:require [kigen.pbr]
+  (:require [kigen.pbr :as pbr]
             [kigen.transf :as transf]
             [kigen.perm :as perm]
-            [kigen.holonomy]
-            [kigen.poset]
-            [kigen.sgp]
-            [kigen.multab]
-            [kigen.igs]
-            [kigen.orbit])
+            [kigen.holonomy :as holonomy]
+            [kigen.poset :as poset]
+            [kigen.sgp :as sgp]
+            [kigen.multab :as multab]
+            [kigen.igs :as igs]
+            [kigen.orbit :as orbit]
+            [kigen.morphism :as morphism])
   (:gen-class))
 
 (defn -main
