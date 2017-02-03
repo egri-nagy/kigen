84478cf4f248ba89bda0db66bd1ec34f139b70cd
Attila Egri-Nagy <attila@egri-nagy.hu>
2017-02-03 22:27:27 +0900

    reformatting condition

--- src/kigen/morphism.clj
+++ src/kigen/morphism.clj
@@ -11,30 +11,26 @@
   "Decides whether the mapping hom from S to T is homomorphic or not."
   [S T hom dom cod]
   (letfn [(f [x y] (let [z (at S x y)
-                           XY (at T (hom x) (hom y))]
-                       (or
-                                        ;both defined but not matching
-                        (and (contains? cod XY)
-                             (contains? dom z)
-                             (not= XY (hom z)))
-                                        ;finishing but XY not defined
-                        (and (= (count dom) (count S))
-                             (not (contains? cod XY))))))]
+                         XY (at T (hom x) (hom y))]
+                     (if (contains? cod XY)
+                       (and (contains? dom z) (not= XY (hom z)))
+                       (= (count dom) (count S)))))]
       (nil? (first (for [x dom y dom :when (f x y)] [x y])))))
 
 (defn morphisms
   "S source multab
    T target multab
   hom - (partial) morphism
-  morphic? - predicate deciding whether a map is a (potential) morphism or not
-  choices - the possible next elements based on partial solution and source"
+  morphic? - predicate deciding whether a map is a (potential) morphism or not"
   [S,T,hom, morphic?]
   (let [Tset (set (range (count T)))]
-    (letfn [(backtrack  [hom dom cod]
+    (letfn [(backtrack [hom dom cod]
               (if (= (count hom) (count S))
                 [hom]
                 (let [ndom (conj dom (count hom))
-                      extensions (map (fn [x] [(conj hom x) (conj cod x)]) Tset)
+                      extensions (map
+                                  (fn [x] [(conj hom x) (conj cod x)])
+                                  Tset)
                       morphic-extensions (filter
                                           (fn [[nhom ncod]]
                                             (morphic? S T nhom ndom ncod))
