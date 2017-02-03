832bd6b73391bd353de5e8ad4ab8bdb2d65e0a05
Attila Egri-Nagy <attila@egri-nagy.hu>
2017-02-03 21:32:36 +0900

    rethinking the morphic? predicate

--- src/kigen/morphism.clj
+++ src/kigen/morphism.clj
@@ -9,20 +9,36 @@
 
 (defn morphic?
   "Decides whether the mapping hom from S to T is homomorphic or not."
-  [S T hom]
-  (let [dom (vec (range (count hom)))
-        cod (set hom)]
-    (letfn [(f [x y] (let [z (at S x y)
+  [S T hom dom cod]
+  (letfn [(f [x y] (let [z (at S x y)
                            XY (at T (hom x) (hom y))]
                        (or
-                        ;both defined but not matching
+                                        ;both defined but not matching
                         (and (contains? cod XY)
-                                (contains? dom z)
-                                (not= XY (hom z)))
-                        ;finishing but XY not defined
+                             (contains? dom z)
+                             (not= XY (hom z)))
+                                        ;finishing but XY not defined
                         (and (= (count dom) (count S))
-                                (not (contains? cod XY))))))]
-      (nil? (first (for [x dom y dom :when (f x y)] [x y]))))))
+                             (not (contains? cod XY))))))]
+      (nil? (first (for [x dom y dom :when (f x y)] [x y])))))
+
+(defn morphisms
+  "S source multab
+   T target multab
+  hom - (partial) morphism
+  morphic? - predicate deciding whether a map is a (potential) morphism or not
+  choices - the possible next elements based on partial solution and source"
+  [S,T,hom, morphic?]
+  (let [Tset (set (range (count T)))]
+    (letfn [(backtrack  [hom dom cod]
+              (if (= (count hom) (count S))
+                [hom]
+                (let [ndom (conj dom (count hom))
+                      extensions (filter
+                                  #(morphic? S T (conj hom %) ndom (conj cod %))
+                                  Tset)] 
+                  (mapcat #(backtrack (conj hom %) ndom (conj cod %))  extensions))))]
+      (backtrack hom (vec (range (count hom))) (set hom)))))
 
 (defn isomorphic?
   "Decides whether the mapping hom from S to T is homomorphic or not."
@@ -35,26 +51,6 @@
                          (= XY (hom z))
                          (not (contains? cod XY)))))]
       (nil? (first (for [x dom y dom :when (not (f x y))] [x y]))))))
-
-
-(defn morphisms
-  "S source multab
-   T target multab
-  hom - (partial) morphism
-  morphic? - predicate deciding whether a map is a (potential) morphism or not
-  choices - the possible next elements based on partial solution and source"
-  [S,T,hom,  morphic?]
-  (let [Tset (set (range (count T)))]
-    (letfn [(backtrack  [hom]
-              (if (= (count hom) (count S))
-                [hom]
-                (let [extensions (filter
-                                  #(morphic? S T %)
-                                  (map #(conj hom %) Tset))] 
-                  (mapcat backtrack  extensions))))]
-      (backtrack hom))))
-
-
 
 (defn isomorphisms
   "S source multab
