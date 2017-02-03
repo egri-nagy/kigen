1f2044ec1db5f1eeddd9b471c55a115f800c7e6c
Attila Egri-Nagy <attila@egri-nagy.hu>
2017-02-03 19:14:18 +0900

    isomorphism checking added as well

--- src/kigen/morphism.clj
+++ src/kigen/morphism.clj
@@ -24,6 +24,19 @@
                                 (not (contains? cod XY))))))]
       (nil? (first (for [x dom y dom :when (f x y)] [x y]))))))
 
+(defn isomorphic?
+  "Decides whether the mapping hom from S to T is homomorphic or not."
+  [S T hom]
+  (let [dom (vec (range (count hom)))
+        cod (set hom)]
+    (letfn [(f [x y] (let [z (at S x y)
+                           XY (at T (hom x) (hom y))]
+                       (if  (contains? dom z)
+                         (= XY (hom z))
+                         (not (contains? cod XY)))))]
+      (nil? (first (for [x dom y dom :when (not (f x y))] [x y]))))))
+
+
 (defn morphisms
   "S source multab
    T target multab
@@ -40,6 +53,8 @@
                                   (map #(conj hom %) Tset))] 
                   (mapcat backtrack  extensions))))]
       (backtrack hom))))
+
+
 
 (defn isomorphisms
   "S source multab
