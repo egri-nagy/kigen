c3e0f5c1bf436774239c6334fe0a9ff7c2e40249
Dersu Uzala <attila@egri-nagy.hu>
2017-02-04 10:48:23 +0900

    dependency update

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "GNU GENERAL PUBLIC LICENSE"
             :url "http://www.gnu.org/copyleft/gpl.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/math.combinatorics "0.1.3"]
+                 [org.clojure/math.combinatorics "0.1.4"]
                  [slamhound "1.5.5"]
                  [proto-repl "0.3.1"]]
   :plugins [[lein-cloverage "1.0.6"]
