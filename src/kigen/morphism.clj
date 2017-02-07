a8524b7c77a87f317bec266394781c4e10b6f1bc
Attila Egri-Nagy <attila@egri-nagy.hu>
2017-02-07 09:08:18 +0900

    one backtrack for all types of morphisms

--- src/kigen/morphism.clj
+++ src/kigen/morphism.clj
@@ -25,56 +25,39 @@
                               (not (subset? XY (hom xy))))))]
     (nil? (first (for [x dom y dom :when (fail? x y)] [x y])))))
 
+(defn isomorphic?
+  "Decides whether the mapping hom from S to T is homomorphic or not."
+  [S T hom dom cod]
+  (letfn [(good? [x y] (let [z (at S x y)
+                             XY (at T (hom x) (hom y))]
+                         (if  (contains? dom z)
+                           (= XY (hom z))
+                           (not (contains? cod XY)))))]
+    (nil? (first (for [x dom y dom :when (not (good? x y))] [x y])))))
+
+(defn one-to-many [targets x] targets)
+(defn one-to-one [targets x] (disj  targets x))
+
 (defn morphisms
   "S source multab
    T target multab
   hom - (partial) morphism
   morphic? - predicate deciding whether a map is a (potential) morphism or not
   targets - subset of T that are possible targets of the morphism"
-  ([S,T,hom, morphic?] (morphisms S T hom morphic? (multab/elts T)))
-  ([S,T,hom, morphic?, targets]
-   (letfn [(backtrack [hom dom cod]
+  ([f, S,T,hom, morphic?] (morphisms f S T hom morphic? (multab/elts T)))
+  ([f, S,T,hom, morphic?, targets]
+   (letfn [(backtrack [hom dom cod targets]
              (if (= (count hom) (count S))
                [hom]
                (let [ndom (conj dom (count hom))
                      extensions (map
-                                 (fn [x] [(conj hom x) (conj cod x)])
+                                 (fn [x] [(conj hom x) (conj cod x) (f targets x)])
                                  targets)
                      morphic-extensions (filter
                                          (fn [[nhom ncod]]
                                            (morphic? S T nhom ndom ncod))
                                          extensions)]
                  (mapcat
-                  (fn [[nhom ncod]] (backtrack nhom ndom ncod))
+                  (fn [[nhom ncod tgs]] (backtrack nhom ndom ncod tgs))
                   morphic-extensions))))]
-     (backtrack hom (vec (range (count hom))) (set hom)))))
-
-(defn isomorphic?
-  "Decides whether the mapping hom from S to T is homomorphic or not."
-  [S T hom]
-  (let [dom (vec (range (count hom)))
-        cod (set hom)]
-    (letfn [(good? [x y] (let [z (at S x y)
-                           XY (at T (hom x) (hom y))]
-                       (if  (contains? dom z)
-                         (= XY (hom z))
-                         (not (contains? cod XY)))))]
-      (nil? (first (for [x dom y dom :when (not (good? x y))] [x y]))))))
-
-(defn isomorphisms
-  "S source multab
-   T target multab
-  hom - (partial) morphism
-  morphic? - predicate deciding whether a map is a (potential) morphism or not
-  choices - the possible next elements based on partial solution and source"
-  [S,T,hom,  morphic?]
-  (let [Tset (multab/elts T)]
-    (letfn [(backtrack  [hom choices]
-              (if (= (count hom) (count S))
-                [hom]
-                (let [valid-choices (remove (set hom) choices)
-                      extensions (filter
-                                  #(morphic? S T %)
-                                  (map #(conj hom %) valid-choices))]
-                  (mapcat #(backtrack % choices)  extensions))))]
-      (backtrack hom Tset))))
+     (backtrack hom (vec (range (count hom))) (set hom) targets))))
