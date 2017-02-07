b07eb741bc011226c804892b09a177f30a8d2c72
Dersu Uzala <attila@egri-nagy.hu>
2017-02-07 18:58:57 +0900

    adds 100% test coverage for morphism.clj

---
+++ test/kigen/morphism_test.clj
@@ -0,0 +1,15 @@
+(ns kigen.morphism-test
+  (:require [clojure.test :refer :all]
+            [kigen.morphism :refer :all]
+            [kigen.multab :as mt]
+            [kigen.sgp :as sgp]
+            [kigen.transf :as transf]))
+
+(deftest test-multab
+  (testing "Testing multiplication tables."
+    (let [mtT2 (mt/multab
+                (sgp/sgp-by-gens (transf/full-ts-gens 2)))]
+      (is (= 120 (count (relmorphisms mtT2 mtT2))))
+      (is (= 2 (count (divisions mtT2 mtT2))))      
+      (is (= 2 (count (isomorphisms mtT2 mtT2))))
+      (is (= 7 (count (homomorphisms mtT2 mtT2)))))))
