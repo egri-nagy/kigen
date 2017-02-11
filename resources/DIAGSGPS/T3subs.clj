2e607775a257f2391c16afbaa111e83edef4483d
Attila Egri-Nagy <attila@egri-nagy.hu>
2017-02-11 09:26:45 +0900

    updating T3 subs script

--- resources/DIAGSGPS/T3subs.clj
+++ resources/DIAGSGPS/T3subs.clj
@@ -1,4 +1,8 @@
-(def T3 (kigen.sgp/sgp-by-gens (kigen.transf/full-ts-gens 3)))
-(def mtT3 (kigen.multab/multab T3))
+(def T3 (kigen.sgp/sgp-by-gens
+         (kigen.transf/full-ts-gens 3)
+         kigen.transf/mul))
+(def mtT3 (kigen.multab/multab
+           T3
+           kigen.transf/mul))
 
 (println (kigen.multab/subsgps mtT3))
