b433adff054007183e372db023a9797158068275
Attila Egri-Nagy <attila@egri-nagy.hu>
2017-02-22 14:14:49 +0900

    renamings

--- src/kigen/morph2.clj
+++ src/kigen/morph2.clj
@@ -4,33 +4,35 @@
             [clojure.set :refer [subset? difference]]
             [clojure.math.combinatorics :refer [subsets partitions]]))
 
-(declare sxt extendt)
+(declare morph extend-by-gen extend-by-all-gens)
 
-(defn f [phi Smul Tmul]
+(defn morph [phi Smul Tmul]
   (let [gens (keys phi)]
     (loop [env {:phi phi :stack (vec gens)}]
       (if (empty? (:stack env))
         (:phi env)
-        (let [nenv (extendt env (peek (:stack env)) gens Smul Tmul)]
+        (let [nenv (extend-by-all-gens env (peek (:stack env)) gens Smul Tmul)]
           (if (nil? nenv)
             nil
             (recur {:phi (:phi nenv) :stack (into (pop (:stack env)) (:stack nenv))})))))))
 
-(defn extendt [env a gens Smul Tmul]
+(defn extend-by-all-gens [env a gens Smul Tmul]
   (let [phi (:phi env)
         stack []]
     (if (empty? gens)
       env
-      (let [p (sxt phi a  (first gens) Smul Tmul)]
+      (let [p (extend-by-gen phi a (first gens) Smul Tmul)]
         (cond (nil? p) nil
               (empty? p) (recur {:phi phi :stack stack} a  (rest gens) Smul Tmul)
               :else (recur {:phi (conj phi p) :stack (conj stack (first p))} a (rest gens) Smul Tmul))))))
 
-;; single element extended by a single generator
-(defn sxt
-  "phi - morphism represented as a map
-  a - element in frontline
-  b - generator of S"
+(defn extend-by-gen
+  "Extends a single element of S by a single generator.
+  phi - morphism represented as a map
+  a - element to be extended
+  b - generator of S
+  returns nil if it is not homomorphic, [] when homomorphic but no new element,
+  [ab AB] where AB is the newly assigned image of the product ab"
   [phi a b mulS mulT]
   (let [ab (mulS a b)
         AB (mulT (phi a) (phi b))]
