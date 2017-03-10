861a53fc78533a1be5130836dcea07ee1313b5c0
Attila Egri-Nagy <attila@egri-nagy.hu>
2017-03-10 14:50:12 +0900

    a function to compile all the Tn into Tn+1 embeddings

--- src/kigen/genmorph.clj
+++ src/kigen/genmorph.clj
@@ -161,6 +161,17 @@
   (let [pairs (for [i (range 1 (inc 7)) j (range 1 (inc i)) ] [j i])]
     (map (fn [[m n]] [m n (count (Tm->Tn m n))]) pairs)))
 
+;;just print this
+(defn Tn->Tn+1 [maxdeg]
+  (letfn  [(incvec [v] (mapv inc v))]
+    (apply str
+           (mapcat
+            #(for [ms (Tm->Tn % (inc %))]
+               (str (apply str
+                           (map (fn [m] (str (incvec (first m)) "->" (incvec (second m)) "\n"))
+                                ms)) "\n") )
+            (range 1 (inc maxdeg))))))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Cayley graph morph matching - next 3 functions are nested, top to bottom ;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
