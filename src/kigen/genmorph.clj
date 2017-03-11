4f2c27a452d709b496ffd944e35391b623fc3c42
Attila Egri-Nagy <attila@egri-nagy.hu>
2017-03-11 16:03:21 +0900

    moving scripts out

--- src/kigen/genmorph.clj
+++ src/kigen/genmorph.clj
@@ -149,29 +149,6 @@
           (println (inc n) (count nmcprs) "morph(s)")
           (recur (inc n) nmcprs))))))
 
-(defn Tm->Tn [m n]
-  (let [Tmgens (transf/full-ts-gens m)
-        result (embeddings-conj Tmgens transf/mul
-                                (transf/full-ts-gens n) transf/mul
-                                (transf/sgp-by-gens
-                                 (transf/symmetric-gens n)))]
-    result))
-
-(defn Tm->Tn-table []
-  (let [pairs (for [i (range 1 (inc 7)) j (range 1 (inc i)) ] [j i])]
-    (map (fn [[m n]] [m n (count (Tm->Tn m n))]) pairs)))
-
-;;just print this
-(defn Tn->Tn+1 [maxdeg]
-  (letfn  [(incvec [v] (mapv inc v))]
-    (apply str
-           (mapcat
-            #(for [ms (Tm->Tn % (inc %))]
-               (str (apply str
-                           (map (fn [m] (str (incvec (first m)) "->" (incvec (second m)) "\n"))
-                                ms)) "\n") )
-            (range 1 (inc maxdeg))))))
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Cayley graph morph matching - next 3 functions are nested, top to bottom ;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
