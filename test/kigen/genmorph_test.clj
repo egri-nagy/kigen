18d3384e9e541285de338536860a0d57de9e5fea
Attila Egri-Nagy <attila@egri-nagy.hu>
2017-03-14 10:27:19 +0900

    adding small test for genmorphs

---
+++ test/kigen/genmorph_test.clj
@@ -0,0 +1,20 @@
+(ns kigen.genmorph-test
+  (:require [clojure.test :refer :all]
+            [kigen.genmorph :refer :all]
+            [kigen.transf :as transf]))
+
+(deftest test-multab
+  (testing "Testing embeddings by generators."
+    (let [T2gens (transf/full-ts-gens 2)
+          T3gens (transf/full-ts-gens 3)
+          S3 (transf/sgp-by-gens (transf/symmetric-gens 3))
+          tmul transf/mul
+          tconj transf/conjugate]
+      (is (= 6 (count
+                (sgp-embeddings-by-gens T3gens tmul
+                                        T3gens tmul ))))
+      (is (= 1 (count
+                (sgp-embeddings-by-gens T3gens tmul
+                                        T3gens tmul
+                                        tconj S3)))))))
+      
