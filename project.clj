6f8ddf0d5ae622a12bd31b6df6447f46940ac0d1
Dersu Uzala <attila@egri-nagy.hu>
2017-03-18 22:20:03 +0900

    new version v17.03.18

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject kigen "17.03.17-DEV"
+(defproject kigen "17.03.18"
   :description "Computational semigroup theory software system shadowing several semigroup packages of the GAP computer algebra system."
   :url "https://github.com/egri-nagy/kigen"
   :license {:name "GNU GENERAL PUBLIC LICENSE"
