738fa1a0775a61223cdd1aac7311d854424c76d3
Dersu Uzala <attila@egri-nagy.hu>
2017-03-20 18:18:48 +0900

    rewriting experiment script  (less memory use by using reduce); and adding instructions

--- experiments/DIAGSGPS/T8conjreps.clj
+++ experiments/DIAGSGPS/T8conjreps.clj
@@ -1,6 +1,19 @@
-;; A001372
+;; Finding all conjugacy class representative transformations of the full
+;; transformation semigroup Tn.
+;; Exploiting the fact that the maximal conjrep is of the form
+;; [1 2 3 .. n-1 0]
+;; (for the cyclic permutation of n points) we enumerate all transformations
+;; lexicographically, odometer style.
+;; recreating integer sequence A001372
 
-(defn nxt [v m]
+;; m is the maximal available digit, e.g. 1 for binary, 9 for decimal
+
+(defn nxt
+  "Returns the next vector in lexicographic ordering.
+  Leftmost is the most significant digit.
+  Replacing the the first maximal digits with 0s, increase the 1st of
+  the remaining digits, then copy leftover."
+  [v m]
   (let [[ms others] (split-with #(= % m) v)]
     (vec (concat
           (repeat (count ms) 0)
@@ -10,15 +23,36 @@
              [(inc (first others))]
              (rest others)))))))
 
+(defn zerovec
+  "A vector containing m+1 zeroes."
+  [m]
+  (vec (repeat (inc m) 0)))
 
-(defn zerovec [m] (vec (repeat (inc m) 0)))
-(defn maxcnj [m] (vec (concat (range 1 (inc m)) [0])))
+(defn maxcnj
+  "Maximal conjugacy class representative for the full transformation semigroup
+  of degree m+1."
+  [m]
+  (vec (concat (range 1 (inc m)) [0])))
 
-(defn g [m] (conj (distinct
-                   (map
-                    (comp t/conjrep reverse)
-                    (take-while #(not (= (vec  (reverse %)) (maxcnj m)))
-                                (iterate #(nxt % m) (zerovec m)))))
-                  (maxcnj m)))
+(defn full-Tm+1-conjreps
+  [m]
+  (let [reps (reduce
+              (fn [coll v] (conj coll ((comp t/conjrep reverse) v)))
+              #{}
+              (take-while #(not (= (vec  (reverse %)) (maxcnj m)))
+                          (iterate #(nxt % m) (zerovec m))))]
+    (conj reps (maxcnj m)))) ; not to forget the maximal conjrep
 
-(spit "x" (vec (g 3)))
+;; checking:
+;; (map (comp count full-Tm+1-conjreps) [0 1 2 3 4 5])
+;; should produce
+;; (1 3 7 19 47 130)
+;; in  a couple of seconds
+
+;; THE REAL CALCULATION - about an hour
+(spit "experiments/DIAGSGPS/T8conjreps" (vec (sort (full-Tm+1-conjreps 7))))
+
+;; this can be slurped back by
+;; (def T8conjreps (load-file "experiments/DIAGSGPS/T8conjreps"))
+
+
