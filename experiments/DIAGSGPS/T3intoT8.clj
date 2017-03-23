ba83b307c444ea124fa1ade7e85d2b5cc88b811b
Attila Egri-Nagy <attila@egri-nagy.hu>
2017-03-23 10:53:32 +0900

    corrected experiment

--- experiments/DIAGSGPS/T3intoT8.clj
+++ experiments/DIAGSGPS/T3intoT8.clj
@@ -1,4 +1,5 @@
 ;; embedding T3 into T8
+;; computed 736 distinct embeddings 2017.03.23
 
 (def T8conjreps (clojure.edn/read-string
                  (slurp "experiments/DIAGSGPS/T8conjreps")))
@@ -8,16 +9,17 @@
 (def index-period-lookup
   (group-by #(sgp/index-period % t/mul) T8conjreps))
 
-(def T3gens (full-ts-gens 3))
+(def T3gens (t/full-ts-gens 3))
+(def T3ips (map #(sgp/index-period % t/mul) T3gens))
 
-(def targets (concat  [(index-period-lookup (first T3gens))]
-                      (map 
+(def targets (concat  [(index-period-lookup (first T3ips))]
+                      (map
                        (fn [gen] (mapcat (fn [cr] (distinct (map #(t/conjugate cr %) S8)))
                                          (index-period-lookup gen)))
-                       (rest T3gens))))
+                       (rest T3ips))))
 
 (println (map count targets))
 
-;; (println
-;;  (count
-;;   (gmorph/embeddings-conj T3gens t/mul targets t/mul t/conjugate S8)))
+(println
+  (count
+   (gmorph/embeddings-conj T3gens t/mul targets t/mul t/conjugate S8)))
