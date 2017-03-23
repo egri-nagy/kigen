68fe4671161144d2085a51f96ab33f5ba5e2e3a6
Attila Egri-Nagy <attila@egri-nagy.hu>
2017-03-23 13:03:14 +0900

    making script more general

---
+++ experiments/DIAGSGPS/TnintoT8.clj
@@ -0,0 +1,30 @@
+;; embedding T3 into T8
+;; computed 736 distinct embeddings 2017.03.23 for T3 -> T8
+
+
+(def T8conjreps (clojure.edn/read-string
+                 (slurp "experiments/DIAGSGPS/T8conjreps")))
+
+(def S8 (t/sgp-by-gens (t/symmetric-gens 8)))
+
+(def index-period-lookup
+  (group-by #(sgp/index-period % t/mul) T8conjreps))
+
+(def n 4)
+
+(def Tngens (t/full-ts-gens n))
+(def Tnips (map #(sgp/index-period % t/mul) Tngens))
+
+(def targets (concat  [(index-period-lookup (first Tnips))]
+                      (map
+                       (fn [gen] (mapcat (fn [cr] (distinct
+                                                   (map #(t/conjugate cr %)
+                                                        S8)))
+                                         (index-period-lookup gen)))
+                       (rest Tnips))))
+
+(println (map count targets))
+
+(println
+  (count
+   (gmorph/embeddings-conj Tngens t/mul targets t/mul t/conjugate S8)))
