b0f6568c51f4745ab61509df9ab0880ba27763c6
Attila Egri-Nagy <attila@egri-nagy.hu>
2017-08-11 22:15:07 +0900

    rearranging code a bit

--- experiments/SUBSEMI/subsgp_conjreps.clj
+++ experiments/SUBSEMI/subsgp_conjreps.clj
@@ -32,35 +32,38 @@
                                                                          crf)))))
 
 (defn subsgps-up-to-conjugacy2 [S G]
-  (let [vS (vec (sort S))
-        n (count vS)
-        indices (vec (range n))
-        t2i (clojure.set/map-invert (zipmap (range n) vS))
-
-        Ghom (fn [p] (mapv t2i (map #(t/conjugate % p) vS)))
-        H (map Ghom G)
-        cf (fn [x p] (p x))
-        conjreps (zipmap indices
-                         (map (fn [x] (t2i (t/conjrep (vS x))))
-                          indices))
-        minconjs (zipmap indices
-                         (map (fn [x] (set
-                                       (second
-                                        (conjugacy/minconjugators cf x H))))
-                              indices))
-        crf (fn [sub]
-              (let [conjugators (reduce
-                                 (fn [[m mcjs] x]
-                                   (let [xrep (conjreps x)
-                                         flag (compare xrep m)]
-                                     (cond (neg? flag) [xrep (minconjs x)]
-                                           (zero? flag) [m (into mcjs (minconjs x))]
-                                           :else [m mcjs])))
-                                        [(inc n) #{}]
-                                        sub)]
-                (i/int-set (conjugacy/setconjrep cf (seq sub) (second conjugators)))))
-        mtS (mt/multab vS t/mul)]
-    (print indices)
+  (let
+      [vS (vec (sort S))
+       n (count vS)
+       indices (vec (range n))
+       t2i (clojure.set/map-invert (zipmap (range n) vS))
+       ;; turning conjugation into simple action
+       Ghom (fn [p] (mapv t2i (map #(t/conjugate % p) vS)))
+       H (map Ghom G)
+       cf (fn [x p] (p x))
+       ;; mapping indices to the index of the conjugacy rep
+       conjreps (zipmap indices
+                        (map (fn [x] (t2i (t/conjrep (vS x))))
+                             indices))
+       ;; index to its minimal conjugators
+       minconjs (zipmap indices
+                        (map (fn [x] (set
+                                      (second
+                                       (conjugacy/minconjugators cf x H))))
+                             indices))
+       ;; set-wise conjugacy class rep function for subsets of indices
+       crf (fn [sub]
+             (let [conjugators (reduce
+                                (fn [[m mcjs :as r] x]
+                                  (let [xrep (conjreps x)
+                                        flag (compare xrep m)]
+                                    (cond (neg? flag) [xrep (minconjs x)]
+                                          (zero? flag) [m (into mcjs (minconjs x))]
+                                          :else r)))
+                                [(inc n) #{}]
+                                sub)]
+               (i/int-set (conjugacy/setconjrep cf (seq sub) (second conjugators)))))
+       mtS (mt/multab vS t/mul)]
     (orbit/full-orbit-parallel [(i/int-set)]
                                (fn [sub] (min-extensions-up-to-conjugacy mtS
                                                                          indices
