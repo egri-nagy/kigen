2dc48cb6872a5e2c4d8b439528b3f8af420152ea
Attila Egri-Nagy <attila@egri-nagy.hu>
2017-08-11 16:31:35 +0900

    better version of subsgp conjugacy

--- experiments/SUBSEMI/subsgp_conjreps.clj
+++ experiments/SUBSEMI/subsgp_conjreps.clj
@@ -36,11 +36,35 @@
         mtS (mt/multab vS t/mul)
         n (count vS)
         t2i (clojure.set/map-invert (zipmap (range n) vS))
+        elts (mapv t2i vS)
         Ghom (fn [p] (mapv t2i (map #(t/conjugate % p) vS)))
+        H (map Ghom G)
         syms (map Ghom G)
         cf (fn [x p] (p x))
-        minconjs (zipmap (mt/elts mtS)
-                         (map (fn [x] (conjugacy/minconjugators cf x G))
-                              (mt/elts mtS)))
-]
-    minconjs))
+        conjreps (zipmap elts
+                         (map (fn [x] (t2i (t/conjrep (vS x))))
+                          elts))
+        minconjs (zipmap elts
+                         (map (fn [x] (set
+                                       (second
+                                        (conjugacy/minconjugators cf x H))))
+                              elts))
+        crf (fn [sub]
+              (let [conjugators (reduce
+                                 (fn [[m mcjs] x]
+                                   (let [xrep (conjreps x)
+                                         flag (compare xrep m)]
+                                     (cond (neg? flag) [xrep (minconjs x)]
+                                           (zero? flag) [m (into mcjs (minconjs x))]
+                                           :else [m mcjs])))
+                                        [(inc n) #{}]
+                                        sub)]
+                (i/int-set (conjugacy/setconjrep cf (seq sub) (second conjugators)))))]
+    (orbit/full-orbit-parallel [(i/int-set)]
+                               (fn [sub] (min-extensions-up-to-conjugacy mtS
+                                                                         elts
+                                                                         sub
+                                                                         crf)))))
+
+(def S3 (t/sgp-by-gens (t/symmetric-gens 3)))
+(def T3 (t/sgp-by-gens (t/full-ts-gens 3)))
