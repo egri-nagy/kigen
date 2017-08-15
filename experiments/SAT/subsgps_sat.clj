8928efefcaf427ad0e5a0f922d2db1caed1a4c7b
Attila Egri-Nagy <attila@egri-nagy.hu>
2017-08-15 09:53:43 +0900

    adding gentab based CNF

--- experiments/SAT/subsgps_sat.clj
+++ experiments/SAT/subsgps_sat.clj
@@ -7,3 +7,12 @@
       [(- (inc i)) (- (inc j)) (inc (multab/at mt i j))])))
 
 ;;(map (partial filter pos?) (sat/solutions mt))
+
+(defn betterCNF [gens]
+  (let [gt (gentab/gentab gens t/mul)
+        m (count (:gens gt))
+        n (count (:tab gt))
+        tab (:tab gt)]
+    (for [i (range n) j (range m)]
+      [(- (inc i)) (- (inc j)) (inc (multab/at  tab i j))])))
+
