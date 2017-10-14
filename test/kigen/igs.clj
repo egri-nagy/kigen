d7f2c7e8a37c05236cba16757005cdf1b54719ea
Attila Egri-Nagy <attila@egri-nagy.hu>
2017-10-14 15:51:30 -0400

    adding new test

---
+++ test/kigen/igs.clj
@@ -0,0 +1,15 @@
+(ns kigen.igs-test
+  (:require [clojure.test :refer :all]
+            [clojure.data.int-map :refer [int-set]]
+            [orbit.core :as orbit]
+            [kigen.igs :as igs]
+            [kigen.multab :as multab]
+            [kigen.transf :as transf]))
+
+(deftest num-of-igs-test
+  (testing "Testing S3 for ."
+    (let [mt (multab/multab (transf/sgp-by-gens (transf/symmetric-gens 3))
+                            transf/mul)]
+      (is (= 16 (orbit/full-orbit-bulk
+                 [(int-set)]
+                 (partial igs/min-extensions mt)))))))
