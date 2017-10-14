40cf345b44791b5220065f6b4242c7140dfe0cb5
Attila Egri-Nagy <attila@egri-nagy.hu>
2017-10-14 16:05:17 -0400

    correct tests now

--- test/kigen/igs_test.clj
+++ test/kigen/igs_test.clj
@@ -8,8 +8,13 @@
 
 (deftest num-of-igs-test
   (testing "Testing S3 for ."
-    (let [mt (multab/multab (transf/sgp-by-gens (transf/symmetric-gens 3))
+    (let [mt3 (multab/multab (transf/sgp-by-gens (transf/symmetric-gens 3))
+                             transf/mul)
+          mt4 (multab/multab (transf/sgp-by-gens (transf/symmetric-gens 4))
                             transf/mul)]
-      (is (= 16 (orbit/full-orbit-bulk
-                 [(int-set)]
-                 (partial igs/min-extensions mt)))))))
+      (is (= 16 (count (orbit/full-orbit-bulk
+                        [(int-set)]
+                        (partial igs/min-extensions mt3)))))
+      (is (= 413 (count (orbit/full-orbit-bulk
+                         [(int-set)]
+                         (partial igs/min-extensions mt4))))))))
