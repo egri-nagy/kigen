999ffc2aa9056b07d59421bf46d5b5d02b2edc4b
Attila Egri-Nagy <attila@egri-nagy.hu>
2017-12-05 11:30:05 +0900

    adding comments

--- src/kigen/gentab.clj
+++ src/kigen/gentab.clj
@@ -2,8 +2,11 @@
   "Table for constant time right multiplication by generators."
   (:require [kigen.sgp :refer [sgp-by-gens]]))
 
+;;TODO this has to be spec-ed; why putting the generators in the front?
+
 (defn gentab
-  "Right generation table for semigroup given by generators."
+  "Right generation table for semigroup given by generator elements and
+  multiplication."
   [gens mul]
   (let [S (sgp-by-gens gens mul)
         elts (vec (concat gens (remove (set gens) S)))
