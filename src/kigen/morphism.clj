7975a377a77fa5513065a11e6852a7cc69821284
Attila Egri-Nagy <attila@egri-nagy.hu>
2017-12-21 23:17:05 +0900

    divisions are fine as well

--- src/kigen/morphism.clj
+++ src/kigen/morphism.clj
@@ -47,16 +47,19 @@
                                  (multab/elts T))))))]
     (acyclic-search-single [[]] sa (fn [v] (= (count v) (count S))))))
 
-
 (defn divisions
   "All divisions from S to T."
   [S T]
   (distinct
    (mapcat
-    #(let [cands (set %)]
-       (one-to-1-morphism-search S T []
-                                 relmorphic?
-                                 (fn [x] cands)))
+    (fn [partition]
+      (let [sa (fn [hom]
+             (if (= (count hom) (count S))
+               #{}
+               (let [rts (remove (set hom) partition)]
+                 (set (filter (partial relmorphic? S T)
+                              (map (partial conj hom) rts))))))]
+        (acyclic-search-single [[]] sa (fn [v] (= (count v) (count S))))))
     (big-enough-partitions (multab/elts T) (count S)))))
 
 (defn isomorphisms
@@ -77,34 +80,6 @@
                  (set (filter (partial homomorphic? S T)
                               (map (partial conj hom) rts))))))]
     (acyclic-search-single [[]] sa (fn [v] (= (count v) (count S))))))
-
-;;------------------------------------------------------------------------------
-;; the generic search algorithms
-
-(defn one-to-1-morphism-search
-  "A backtrack search for constructing lossless morphisms from a source
-  multiplication table S to a target T. A partial morphism can be given to
-  constrain the search (TODO the partial part is not done yet) or an empty
-  vector to get all. The predicate function morphic? checks whether a new
-  extension is morphic or not according to the type of the morphism."
-  [S T hom morphic? cand-fn]
-  (letfn [(backtrack [hom dom cod used]
-            (if (= (count hom) (count S))
-              [hom]
-              (let [ndom (conj dom (count hom))
-                    extensions (map ; creating argument vector for recursion
-                                (fn [x] [(conj hom x)
-                                         (conj cod x)
-                                         (conj used x)])
-                                (difference (cand-fn (peek ndom)) used))
-                    morphic-extensions (filter
-                                        (fn [[nhom ncod]]
-                                          (morphic? S T nhom))
-                                        extensions)]
-                (mapcat ; recursion here
-                 (fn [[nhom ncod used]] (backtrack nhom ndom ncod used))
-                 morphic-extensions))))]
-    (backtrack hom (vec (range (count hom))) (set hom) #{})))
 
 ;;------------------------------------------------------------------------------
 ;; Predicates for checking the 'morphicity' of a mapping.
