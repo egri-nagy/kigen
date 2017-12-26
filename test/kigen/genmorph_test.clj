42e63cec04b8296526d9935890d296ae997026dd
Attila Egri-Nagy <attila@egri-nagy.hu>
2017-12-26 23:38:41 +0900

    adding more tests

--- test/kigen/genmorph_test.clj
+++ test/kigen/genmorph_test.clj
@@ -7,7 +7,9 @@
   (testing "Testing embeddings by generators."
     (let [T2gens (transf/full-ts-gens 2)
           T3gens (transf/full-ts-gens 3)
+          T4gens (transf/full-ts-gens 4)
           S3 (transf/sgp-by-gens (transf/symmetric-gens 3))
+          S4 (transf/sgp-by-gens (transf/symmetric-gens 4))
           tmul transf/mul
           tconj transf/conjugate]
       (is (= 6 (count
@@ -20,5 +22,13 @@
       (is (= 3 (count
                 (sgp-embeddings-by-gens T2gens tmul
                                         T3gens tmul
-                                        tconj S3)))))))
+                                        tconj S3))))
+      (is (= 12 (count
+                (sgp-embeddings-by-gens T2gens tmul
+                                        T4gens tmul
+                                        tconj S4))))
+      (is (= 4 (count
+                (sgp-embeddings-by-gens T3gens tmul
+                                        T4gens tmul
+                                        tconj S4)))))))
       
