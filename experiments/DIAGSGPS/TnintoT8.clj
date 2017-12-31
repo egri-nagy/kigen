5289acc0aec9fe75fa4419fe5267633715f2dc2d
Attila Egri-Nagy <attila@egri-nagy.hu>
2017-12-31 16:55:55 +0900

    introducing a function

--- experiments/DIAGSGPS/TnintoT8.clj
+++ experiments/DIAGSGPS/TnintoT8.clj
@@ -4,18 +4,21 @@
 ;; computed 736 distinct embeddings 2017.03.23 for T3 -> T8
 ;; 48 for T4->T8
 
+;; precalculated
 (def T8conjreps (clojure.edn/read-string
                  (slurp "experiments/DIAGSGPS/T8conjreps")))
 
 (def S8 (t/sgp-by-gens (t/symmetric-gens 8)))
 
+(defn t-i-p [x] (sgp/index-period x t/mul))
+
 (def index-period-lookup
-  (group-by #(sgp/index-period % t/mul) T8conjreps))
+  (group-by t-i-p T8conjreps))
 
 (def n 7)
 
 (def Tngens (t/full-ts-gens n))
-(def Tnips (map #(sgp/index-period % t/mul) Tngens))
+(def Tnips (map t-i-p Tngens))
 
 (def targets (concat  [(index-period-lookup (first Tnips))]
                       (map
