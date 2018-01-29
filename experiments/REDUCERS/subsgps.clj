348fa6f8fc61b9c51a30026d26f33b754ba85f34
Attila Egri-Nagy <attila@egri-nagy.hu>
2018-01-29 11:42:54 +0900

    update script

--- experiments/REDUCERS/subsgps.clj
+++ experiments/REDUCERS/subsgps.clj
@@ -1,6 +1,8 @@
 (use '[criterium.core])
 (use '[clojure.data.int-map :as i])
 (use '[orbit.core :as orbit])
+(use '[kigen.multab :as multab])
+(use '[kigen.transf :as t])
 
 (def mtS
   (multab/multab (t/sgp-by-gens [[1 3 0 3] [2 2 1 3] [1 1 2 2]])
@@ -11,20 +13,17 @@
   table"
   [mt]
   (let [elts (multab/elts mt)]
-    (orbit/full-orbit-single [(i/int-set)] (partial multab/min-extensions mt elts))))
+    (orbit/full-orbit [(i/int-set)] (partial multab/min-extensions mt elts))))
 
   (defn subsgps-parallel
     "All subsemigroups of an abstract semigroup given by its multiplication
   table"
     [mt]
     (let [elts (multab/elts mt)]
-      (orbit/full-orbit-parallel [(i/int-set)] (partial multab/min-extensions mt elts))))
+      (orbit/pfull-orbit [(i/int-set)] (partial multab/min-extensions mt elts))))
 
 
 (println "Single")
-
-
-
 
   (bench
    (subsgps-single mtS))
