6a7b6e3d659b8b92dc1344939d034ab9d592c2c6
Attila Egri-Nagy <attila@egri-nagy.hu>
2018-03-02 13:26:47 +0900

    version bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject kigen "18.02.12"
+(defproject kigen "18.03.02"
   :description "Computational semigroup theory software system shadowing several semigroup packages of the GAP computer algebra system."
   :url "https://github.com/egri-nagy/kigen"
   :license {:name "GNU GENERAL PUBLIC LICENSE"
