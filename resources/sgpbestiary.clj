a8ed867d5b4db332607b0dbad74eb83531a39c27
Attila Egri-Nagy <attila@egri-nagy.hu>
2018-03-06 10:56:40 +0900

    adding sgp example

--- resources/sgpbestiary.clj
+++ resources/sgpbestiary.clj
@@ -9,3 +9,6 @@
    [1,2,0,3,3,3]]);makes H({0,1,2}) nontrivial
 
 (def alifex [ [ 1, 1, 2, 2, 2 ], [ 2, 2, 2, 4, 3 ] ])
+
+;; overlapping covers for parallel components
+(def overlapping-covers [[3 2 3 1] [0 1 1 0] [2 1 0 0]])
