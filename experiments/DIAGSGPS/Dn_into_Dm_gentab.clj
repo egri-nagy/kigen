f8f4a6c94aaeb85889649b80e1f005d004feafc4
Attila Egri-Nagy <attila@egri-nagy.hu>
2018-03-09 16:35:38 +0900

    following upstream changes

--- experiments/DIAGSGPS/Dn_into_Dm_gentab.clj
+++ experiments/DIAGSGPS/Dn_into_Dm_gentab.clj
@@ -2,32 +2,39 @@
 ;; semigroups of the same type
 ;; generic code for diagram semigroups using the transformation representation
 (require '[kigen.transf :as t]
+         '[kigen.conjugacy :as c]
+         '[kigen.transf-conj :as t-c]
          '[kigen.genmorph :as gmorph]
          '[clojure.pprint :refer [pprint]]
-	 '[taoensso.timbre :as timbre])
+         '[taoensso.timbre :as timbre])
 
 (timbre/merge-config! {:level :info})
 
 (defn Dn-into-Dm [gf n m]
   (let [Tngens (gf n)
         Tmgens (gf m)
-        targets (gmorph/index-period-matched Tngens t/mul
+        targets (gmorph/index-period-matched (gmorph/->Sgp Tngens t/mul)
                                              Tmgens t/mul)]
     (gmorph/embeddings-distinct Tngens t/mul targets t/mul
-                                t/conjrep t/conj-conj t/setconjrep)))
+                                ;;(c/conjugation-fn-bundle
+                                ;;  t/conjugate
+                                ;;  (t/sgp-by-gens (t/symmetric-gens m))))))
+                                (c/->ConjugationFunctionBundle t-c/conjrep
+                                                               t-c/setconjrep
+                                                               t-c/conj-conj))))
 
 (defn Dn->Dm-table [gf n]
   (let [pairs (for [i (range 1 (inc n))
                     j (range 1 (inc i))]
                 [j i])]
     (map (fn [[m n]]
-    	   (let [r (count (Dn-into-Dm gf m n))]
+           (let [r (count (Dn-into-Dm gf m n))]
              (println \# m n r)
              [[m n] r]))
          pairs)))
 
 (binding [orbit.extension/*task-size* 1]
-  (def result (Dn-into-Dm t/full-ts-gens 7 8)))
+  (def result (Dn-into-Dm t/full-ts-gens 5 6)))
 
 (println (count result))
 
