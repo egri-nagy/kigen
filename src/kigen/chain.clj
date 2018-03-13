4fb34f425b14441cf21afdad9f356e24498e8427
Attila Egri-Nagy <attila@egri-nagy.hu>
2018-03-13 10:40:27 +0900

    be a bit more precise about the input relation

--- src/kigen/chain.clj
+++ src/kigen/chain.clj
@@ -1,29 +1,30 @@
 (ns kigen.chain
-  "Chains in partially ordered sets."
+  "Chains in partially ordered sets given by their Hasse diagrams.
+  A Hasse diagram  is a hash-map from elements to the set of related elements."
  (:require [orbit.core :refer [tree-search]]
            [clojure.set :refer [union]]))
 
 (defn chains
- "All chains between elements a and b in the given (explicit) cover relation."
-  [a b cr]
+ "All chains between elements a and b in the given Hasse diagram."
+  [a b hd]
   (letfn [(solution? [chain] (= b (last chain)))
           (extensions [chain] ;no heuristics applied, no candidate? function
             (when-not (solution? chain)
-              (map (partial conj chain) (cr (last chain)))))]
+              (map (partial conj chain) (hd (last chain)))))]
     (tree-search [[a]] extensions solution?)))
 
 (defn gaps
   "Returns all pairs of consecutive elements of the chain that are not in
     cover relation."
-  [chain cr]
-  (remove (fn [[a b]] (contains? (cr a) b))
+  [chain hd]
+  (remove (fn [[a b]] (contains? (hd a) b))
           (partition 2 1 chain)))
 
 (defn discovery-times
-  "For a Hasse-diagram/cover relation cr this perform a search and records the
+  "For a Hasse-diagram/cover relation hd this perform a search and records the
     discovery times. Returns a map: element -> vector of discovery times
   f: min or max"
-  [e cr]
+  [e hd]
   (loop [d 0
          dists {}
          frontier #{e}]
@@ -31,11 +32,11 @@
       dists
       (recur (inc d)
              (reduce #(update-in % [%2] conj d) dists frontier)
-             (apply union (map cr frontier))))))
+             (apply union (map hd frontier))))))
 
 (defn max-distances
   "Returns a map of elements to their maximal distance
     (length of longest chain) from element e."
-  [e cr]
+  [e hd]
   (into {} (map (fn [[k v]] [k (apply max v)])
-                (discovery-times e cr))))
+                (discovery-times e hd))))
