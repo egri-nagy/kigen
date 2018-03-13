665fdc91490a54d85b70e4b107756f9564988102
Attila Egri-Nagy <attila@egri-nagy.hu>
2018-03-13 15:04:26 +0900

    adding docstrings

--- src/kigen/chain_sgp.clj
+++ src/kigen/chain_sgp.clj
@@ -17,6 +17,8 @@
 
 
 (defn on-hd
+  "The result of acting by a transformation on superset Hasse diagram in a
+  skeleton. The full state set and the singletons are put back."
   [hd X transf]
   (let [transformed (set (map #(t/act % transf) (keys hd)))
         elts (-> transformed
@@ -34,12 +36,16 @@
             [k v])))
 
 (defn fillings
+  "A map that contains filling for gaps in a reduced Hasse diagrams. Used
+  for finding dominating chains."
   [hd fullhd]
   (into {} (for [g (gaps hd fullhd)]
              [g ((comp butlast rest first) ;choice is made here, we pick the 1st
                  (chain/chains (first g) (second g) fullhd))])))
 
 (defn on-max-chain
+  "Acting on  maximal chain. Finding a dominating chain by using a fillings
+  table."
   [c t fllngs]
   (let [rc (distinct (map #(t/act % t) c))
         rrc (if (= (last c) (last rc))
@@ -52,18 +58,20 @@
             rrc)))
 
 (defn ->transf
+  "A transformation encoding the action of a transformation on all maximal
+  chains."
   [sk t]
   (let [m-chains (max-chains sk)
         sorted-max-chains  (sort (fn [x y] (compare (mapv vec x) (mapv vec y)))
                                  m-chains);compare as vectors
         nhd  (on-hd (sk :supsethd) (sk :stateset) t)
         fngs (fillings nhd (:supsethd sk))]
-    (println sorted-max-chains)
     (mapv (fn [c] (pos/index sorted-max-chains
                              (on-max-chain c t fngs)))
           sorted-max-chains)))
 
 (defn chain-sgp-gens
+  "Just a convenient function to map generators to chain semigroup generators."
   [gens]
   (map (partial ->transf (sk/skeleton gens))
        gens))
