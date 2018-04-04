fa7b076f60e181563b2f1e3da216098467ac6d8c
Attila Egri-Nagy <attila@egri-nagy.hu>
2018-04-04 12:22:54 +0900

    adding more assertions to morphism testing

--- test/kigen/morphism_test.clj
+++ test/kigen/morphism_test.clj
@@ -2,15 +2,26 @@
   (:require [clojure.test :refer :all]
             [kigen.morphism :refer :all]
             [kigen.multab :as mt]
-            [kigen.sgp :as sgp]
             [kigen.transf :as transf]))
 
 (deftest test-multab
-  (testing "Testing multiplication tables."
-    (let [mtT2 (mt/multab
-                (sgp/sgp-by-gens (transf/full-ts-gens 2) transf/mul)
-                transf/mul)]
+  (testing "Testing morphisms by multiplication tables."
+    (let [mtT2 (mt/multab (transf/sgp-by-gens (transf/full-ts-gens 2))
+                          transf/mul)
+          mtS3 (mt/multab (transf/sgp-by-gens (transf/symmetric-gens 3))
+                          transf/mul)]
+      ;T2 -> T2
       (is (= 120 (count (relmorphisms mtT2 mtT2))))
       (is (= 2 (count (divisions mtT2 mtT2))))
       (is (= 2 (count (isomorphisms mtT2 mtT2))))
-      (is (= 7 (count (homomorphisms mtT2 mtT2)))))))
+      (is (= 7 (count (homomorphisms mtT2 mtT2))))
+      ;T2-> S3
+      (is (= 22 (count (relmorphisms mtT2 mtS3))))
+      (is (= 0 (count (divisions mtT2 mtS3))))
+      (is (= 0 (count (isomorphisms mtT2 mtS3))))
+      (is (= 1 (count (homomorphisms mtT2 mtS3))))
+      ;S3 -> S3
+      (is (= 16 (count (relmorphisms mtS3 mtS3))))
+      (is (= 6 (count (divisions mtS3 mtS3))))
+      (is (= 6 (count (isomorphisms mtS3 mtS3))))
+      (is (= 10 (count (homomorphisms mtS3 mtS3)))))))
