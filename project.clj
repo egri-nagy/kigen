f7bf63c416fd4ee26a054bde1822090cd57c7e4c
Dersu Uzala <attila@egri-nagy.hu>
2018-04-21 22:04:56 +0900

    adding rebl repl

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject kigen "18.03.15"
+(defproject kigen "18.04.21"
   :description "Computational semigroup theory software system shadowing several semigroup packages of the GAP computer algebra system."
   :url "https://github.com/egri-nagy/kigen"
   :license {:name "GNU GENERAL PUBLIC LICENSE"
@@ -10,7 +10,8 @@
                  [rolling-stones "1.0.0-SNAPSHOT"]
                  [org.clojure/core.logic "0.8.11"]
                  [com.taoensso/timbre "4.10.0"]
-                 [slamhound "1.5.5"]]
+                 [slamhound "1.5.5"]
+                 [com.bhauman/rebel-readline "0.1.2"]]
   :plugins [[lein-cloverage "1.0.6"]
             [lein-kibit "0.1.2"]
             [lein-ancient "0.6.14"]
@@ -19,4 +20,5 @@
   :main ^:skip-aot kigen.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}}
-  :aliases {"slamhound" ["run" "-m" "slam.hound"]})
+  :aliases {"slamhound" ["run" "-m" "slam.hound"]
+            "rebl" ["trampoline" "run" "-m" "rebel-readline.main"]})
