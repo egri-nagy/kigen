a3d90dee19be02e03749f21ebdaeff3f1d07013e
Attila Egri-Nagy <attila@egri-nagy.hu>
2018-05-10 10:17:16 +0900

    adding script

---
+++ experiments/DIAGSGPS/T3_into_Br8.clj
@@ -0,0 +1,15 @@
+;; counting the embeddings of T3 into Br8
+(require '[kigen.transf :as t]
+	 '[kigen.pbr :as pbr]
+	 '[kigen.sgp :as sgp]
+         '[kigen.genmorph :as gmorph]
+         '[clojure.pprint :refer [pprint]]
+         '[taoensso.timbre :as timbre])
+
+(timbre/merge-config! {:level :info})
+
+(def result (gmorph/sgp-embeddings-by-gens (t/full-ts-gens 3) t/mul
+                                           (pbr/brauer-gens 6) pbr/mul))
+
+(println (count result))
+
