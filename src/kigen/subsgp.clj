338c31f4bcbf0a430d06c9352b47eee66a474454
Attila Egri-Nagy <attila@egri-nagy.hu>
2018-06-18 17:00:08 +0900

    working subsgp (switching computer)

--- src/kigen/subsgp.clj
+++ src/kigen/subsgp.clj
@@ -2,15 +2,30 @@
   "General functions for computing subsemigroups.
   Black box style, the element(s) and the operation need to be supplied."
   (:require [clojure.math.combinatorics :refer [selections]]
-            [orbit.core :refer [full-orbit]]
-            [orbit.action :refer [right-action right-actions set-action]]
+            [orbit.core :refer [full-orbit-single-op full-orbit]]
+            [orbit.action :refer [right-action]]
             [kigen.memory-info :refer [mem-info]]
             [taoensso.timbre :refer [info]]))
 
 
-(defn subsgp-by-closure
+(defn subsgp-closure
   "Adding a single new generator. S - subsemigroup, gen - a new generator,
   mul - the semigroup's binary operation."
   [S gen mul]
-  (full-orbit (conj S gen)
-              (set-action [(right-action mul gen)])))
+  (full-orbit-single-op (conj S gen)
+                        (right-action mul gen)))
+
+(defn min-extensions
+  "Returns the minimal extensions (by new element) of closed subarray of
+  multiplication table mt."
+  [S mul closedsub]
+  (let [X (remove closedsub S)]
+    (println X)
+    (set (map #(subsgp-closure closedsub % mul) X))))
+
+
+(defn subsgps
+  "All subsemigroups of the given semigroup. Subsemigroups will be "
+  [S mul]
+  (full-orbit [(empty S)] ;keeping collection type
+              (partial min-extensions S mul)))
