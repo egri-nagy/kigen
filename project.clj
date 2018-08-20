69901cd6c44ed8594ad953ed3a31c068e6c33bca
Attila Egri-Nagy <attila@egri-nagy.hu>
2018-08-20 13:04:17 +0900

    updating dependency

--- project.clj
+++ project.clj
@@ -11,7 +11,7 @@
                  [org.clojure/core.logic "0.8.11"]
                  [com.taoensso/timbre "4.10.0"]
                  [slamhound "1.5.5"]
-                 [com.bhauman/rebel-readline "0.1.3"]]
+                 [com.bhauman/rebel-readline "0.1.4"]]
   :plugins [[lein-cloverage "1.0.6"]
             [lein-kibit "0.1.2"]
             [lein-ancient "0.6.14"]
