f1c33f4ee77c427ccbc89116733b62f299502c5e
Attila Egri-Nagy <attila@egri-nagy.hu>
2018-09-17 14:38:05 +0900

    adding function for all latin square extensions

--- experiments/PERMPRODSAMPLING/permprods.clj
+++ experiments/PERMPRODSAMPLING/permprods.clj
@@ -13,7 +13,7 @@
 (defn extend-latin-rectangle
   "Extending a partial latin square (latin rectangle) by one more row.
   n - dimension of the rectangle
-  rect - the partial latin square"
+  rect - the partial latin square represented as a vector"
   [n rect]
   ;(print (/ (count rect) n) " ") (flush)
   (let [tab (into rect (vec (repeatedly n l/lvar)))
@@ -26,13 +26,28 @@
              (l/everyg fd/distinct cols)
              (l/== q tab)))))
 
+(defn all-latin-squares
+  "All Latin squares from a partial Latin table."
+  [n partialtab]
+  (let [size (* n n)
+        tab (into partialtab
+                  (vec (repeatedly (- size (count partialtab)) l/lvar)))
+        rows (partition n tab)
+        cols (apply map vector rows)
+        points (fd/interval 0 (dec n))]
+    (l/run* [q]
+      (l/everyg #(fd/in % points) tab)
+      (l/everyg fd/distinct rows)
+      (l/everyg fd/distinct cols)
+      (l/== q tab))))
+
 (defn random-latin-square [n]
   (let [size (* n n)]
     (first
      (drop-while (fn [sol] (not (or (nil? sol)
                                     (= size (count sol)))))
                  (iterate (partial extend-latin-rectangle n)
-                          (rand-perm n))))))
+                      (rand-perm n))))))
 
 (defn product [v]
   (reduce transf/mul v))
@@ -44,6 +59,6 @@
         num-of-tuples (Math/pow m k)]
     (/ num-of-classes num-of-tuples)))
 
-(defn uuid [] (str (java.util.UUID/randomUUID)))
+;(defn uuid [] (str (java.util.UUID/randomUUID)))
 
-(spit (uuid) (random-latin-square 16))
+;(spit (uuid) (random-latin-square 16))
