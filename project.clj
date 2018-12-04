d1b77ba35e4b5c09ec489ed794e56f68c22066d5
Attila Egri-Nagy <attila@egri-nagy.hu>
2018-12-04 10:56:33 +0900

    testing the 1.10 latest RC

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/egri-nagy/kigen"
   :license {:name "GNU GENERAL PUBLIC LICENSE"
             :url "http://www.gnu.org/copyleft/gpl.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
+  :dependencies [[org.clojure/clojure "1.10.0-RC3"]
                  [org.clojure/math.combinatorics "0.1.4"]
                  [org.clojure/data.int-map "0.2.4"]
                  [orbit "18.06.14"]
