66a03b3c913d7e7807a08166f01b86042c16429c
Attila Egri-Nagy <attila@egri-nagy.hu>
2018-12-06 16:55:50 +0900

    better naming of logic variables

--- experiments/CCODING/cc.clj
+++ experiments/CCODING/cc.clj
@@ -14,12 +14,12 @@
 (defn sumo
   "This goal succeeds if adding up the elements of l is sum."
   [l sum]
-  (cl/fresh [a d sum-of-remaining]
+  (cl/fresh [head tail sum-of-remaining]
     (cl/conde
      [(cl/== l ()) (cl/== sum 0)]
-     [(cl/conso a d l)
-      (fd/+ a sum-of-remaining sum)
-      (sumo d sum-of-remaining)])))
+     [(cl/conso head tail l)
+      (fd/+ head sum-of-remaining sum)
+      (sumo tail sum-of-remaining)])))
 
 (defn counto
   "This goal succeeds if l has s number of elements."
@@ -47,15 +47,15 @@
   []
   (let [even (apply fd/domain (range 0 10 2))
         odd (apply fd/domain (range 1 10 2))] ; here we give the possible values
-    (cl/run 4 [q] ; the number of expected solutions (if more given, then it hangs)
+    (cl/run 2 [q] ; the number of expected solutions (if more given, then it hangs)
       (cl/conde
        [(everyo q #(fd/in % even))]
        [(everyo q #(fd/in % odd))])
       (increasingo q 0)
       (cl/conde
        [(sumo q 10)]
-       [(sumo q 11)]) ; sum given here, for 11 no solution expected
-      (counto q 2)))) ; length of vector given here
+       [(sumo q 11)]) ; sum given here
+      (counto q 6 )))) ; length of vector given here
 
 (println
  (find-sols))
