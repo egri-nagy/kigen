5248499fab9d8af05d7d0b07626d0676dfe580c9
Attila Egri-Nagy <attila@egri-nagy.hu>
2020-12-17 13:16:46 +0900

    updating deps

--- project.clj
+++ project.clj
@@ -5,18 +5,18 @@
             :url "http://www.gnu.org/copyleft/gpl.html"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/math.combinatorics "0.1.6"]
-                 [org.clojure/data.int-map "0.2.4"]
+                 [org.clojure/data.int-map "1.0.0"]
                  [orbit "19.08.05"]
                  [rolling-stones "1.0.1"]
-                 [org.clojure/core.logic "0.8.11"]
-                 [com.taoensso/timbre "4.10.0"]
+                 [org.clojure/core.logic "1.0.0"]
+                 [com.taoensso/timbre "5.1.0"]
                  [slamhound "1.5.5"]
                  [com.bhauman/rebel-readline "0.1.4"]]
-  :plugins [[lein-cloverage "1.1.1"]
-            [lein-kibit "0.1.6"]
+  :plugins [[lein-cloverage "1.2.1"]
+            [lein-kibit "0.1.8"]
             [lein-ancient "0.6.15"]
             [lein-bikeshed "0.5.2"]
-            [jonase/eastwood "0.3.6"]]
+            [jonase/eastwood "0.3.12"]]
   :main ^:skip-aot kigen.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}}
