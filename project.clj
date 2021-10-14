dd966bedd2ee6c877c7e3e7f4b7ebbc219b3e485
Attila Egri-Nagy <attila@egri-nagy.hu>
2021-10-14 12:50:54 +0900

    just checking updated deps for regression

--- project.clj
+++ project.clj
@@ -3,20 +3,20 @@
   :url "https://github.com/egri-nagy/kigen"
   :license {:name "GNU GENERAL PUBLIC LICENSE"
             :url "http://www.gnu.org/copyleft/gpl.html"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
+  :dependencies [[org.clojure/clojure "1.10.3"]
                  [org.clojure/math.combinatorics "0.1.6"]
                  [org.clojure/data.int-map "1.0.0"]
                  [orbit "19.08.05"]
                  [rolling-stones "1.0.1"]
                  [org.clojure/core.logic "1.0.0"]
-                 [com.taoensso/timbre "5.1.0"]
+                 [com.taoensso/timbre "5.1.2"]
                  [slamhound "1.5.5"]
                  [com.bhauman/rebel-readline "0.1.4"]]
-  :plugins [[lein-cloverage "1.2.1"]
+  :plugins [[lein-cloverage "1.2.2"]
             [lein-kibit "0.1.8"]
-            [lein-ancient "0.6.15"]
+            [lein-ancient "0.7.0"]
             [lein-bikeshed "0.5.2"]
-            [jonase/eastwood "0.3.12"]]
+            [jonase/eastwood "0.9.9"]]
   :main ^:skip-aot kigen.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}}
