c09d0fccadce74b47c6a20d0d6f0a9633544a8f8
Attila Egri-Nagy <attila@egri-nagy.hu>
2022-03-11 11:27:59 +0900

    updating deps

--- project.clj
+++ project.clj
@@ -8,7 +8,7 @@
                  [org.clojure/data.int-map "1.0.0"]
                  [orbit "19.08.05"]
                  [rolling-stones "1.0.2"]
-                 [org.clojure/core.logic "1.0.0"]
+                 [org.clojure/core.logic "1.0.1"]
                  [com.taoensso/timbre "5.1.2"]
                  [slamhound "1.5.5"]
                  [com.bhauman/rebel-readline "0.1.4"]]
@@ -16,7 +16,7 @@
             [lein-kibit "0.1.8"]
             [lein-ancient "0.7.0"]
             [lein-bikeshed "0.5.2"]
-            [jonase/eastwood "1.2.2"]]
+            [jonase/eastwood "1.2.3"]]
   :main ^:skip-aot kigen.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}}
