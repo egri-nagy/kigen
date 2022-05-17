809d15c250479b83e0fba47e024e271de08540cb
Attila Egri-Nagy <attila@egri-nagy.hu>
2022-05-17 18:34:25 +0900

    pmap based implementation

--- experiments/SUBSEMI/subsgps_minimal_gensets.clj
+++ experiments/SUBSEMI/subsgps_minimal_gensets.clj
@@ -55,10 +55,20 @@
           news (db-filter db exts)
           newdb (extend-db db news)
           nn-l (into next-layer news)]
-      (print (count exts) ">" (count news) ", ") (flush)
+      ;(print (count exts) ">" (count news) ", ") (flush)
       (if (empty? (rest q))
         [newdb nn-l]
         (recur (rest q) newdb nn-l)))))
+
+(defn player ;todo rewrite this to make it parallel
+  "takes a queue a database, and returns an updated db and the newly discovered sgps"
+  [q db mtS crf]
+  (reduce
+   (fn [[db next-layer] exts]
+     (let [news (db-filter db exts)]
+       [(extend-db db news) (into next-layer news)]))
+   [db {}]
+   (pmap #(extend-sub % mtS crf) q)))
 
 (defn subsgps
   [S]
@@ -72,7 +82,7 @@
     (loop [q { (i-m/int-set) (i-m/int-set)}
            db {}
            n 1]
-      (let [[ndb nq] (layer q db mtS crf)]
+      (let [[ndb nq] (player q db mtS crf)]
         (spit (str "db" n) (prn-str ndb))
         (spit (str "gens" n) (prn-str nq))
         (println "#gens: " n "total: " (apply + (map count (vals ndb))) "new: " (count nq))
@@ -82,16 +92,12 @@
 
 ;(def S3 (t/sgp-by-gens (t/symmetric-gens 3)))
                                         ;(def T3 (t/sgp-by-gens (t/full-ts-gens 3)))
-(def T4 (t/sgp-by-gens (t/full-ts-gens 4)))
+(def T3 (t/sgp-by-gens (t/full-ts-gens 3)))
 ;;(clojure.pprint/pprint (subsgps T3))
-(clojure.pprint/pprint  (let [result (subsgps T4)]
-                          (for [k (sort (keys result))]
-                            [k (count (result k))])))
-
 
 ;(def S4 (t/sgp-by-gens (t/symmetric-gens 4)))
 
-;(def K42 (t/sgp-by-gens [ [ 0, 1, 1, 1 ], [ 0, 0, 2, 2 ], [ 2, 0, 0, 2 ], [ 3, 1, 3, 3 ], [ 2, 2, 0, 2 ], [ 0, 3, 0, 3 ], [ 2, 2, 3, 2 ], [ 1, 1, 2, 2 ], [ 2, 2, 2, 0 ] ]))
+(def K42 (t/sgp-by-gens [ [ 0, 1, 1, 1 ], [ 0, 0, 2, 2 ], [ 2, 0, 0, 2 ], [ 3, 1, 3, 3 ], [ 2, 2, 0, 2 ], [ 0, 3, 0, 3 ], [ 2, 2, 3, 2 ], [ 1, 1, 2, 2 ], [ 2, 2, 2, 0 ] ]))
 
 
 ;(time (def T3subs (subsgps-up-to-conjugacy T3)))
@@ -99,3 +105,7 @@
 ;(println (count T3subs) " vs " (count T3subs2))
 
 ;(time (def K42subs2 (subsgps-up-to-conjugacy2 K42 S4)))
+(clojure.pprint/pprint  (let [result (subsgps K42)]
+                          (for [k (sort (keys result))]
+                            [k (count (result k))])))
+
