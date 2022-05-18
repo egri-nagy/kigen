1e372b63ba500ff3a4e4ca460bf1aeef05c4a1b1
Attila Egri-Nagy <attila@egri-nagy.hu>
2022-05-18 18:41:54 +0900

    script cleanup

--- experiments/SUBSEMI/subsgps_minimal_gensets.clj
+++ experiments/SUBSEMI/subsgps_minimal_gensets.clj
@@ -87,22 +87,19 @@
                  ndb
                  (inc n)))))))
 
-;(def S3 (t/sgp-by-gens (t/symmetric-gens 3)))
-                                        ;(def T3 (t/sgp-by-gens (t/full-ts-gens 3)))
-;(def T3 (t/sgp-by-gens (t/full-ts-gens 3)))
-;;(clojure.pprint/pprint (subsgps T3))
+(defn print-result [S G]
+  (clojure.pprint/pprint  (let [result (subsgps S G)]
+                            (for [k (sort (keys result))]
+                              [k (count (result k))]))) )
+
+(def S3 (t/sgp-by-gens (t/symmetric-gens 3)))
+(def T3 (t/sgp-by-gens (t/full-ts-gens 3)))
 
 (def S4 (t/sgp-by-gens (t/symmetric-gens 4)))
 
 (def K42 (t/sgp-by-gens [ [ 0, 1, 1, 1 ], [ 0, 0, 2, 2 ], [ 2, 0, 0, 2 ], [ 3, 1, 3, 3 ], [ 2, 2, 0, 2 ], [ 0, 3, 0, 3 ], [ 2, 2, 3, 2 ], [ 1, 1, 2, 2 ], [ 2, 2, 2, 0 ] ]))
 
 
-;(time (def T3subs (subsgps-up-to-conjugacy T3)))
-;(time (def T3subs2 (subsgps-up-to-conjugacy2 T3 S3)))
-;(println (count T3subs) " vs " (count T3subs2))
-
-;(time (def K42subs2 (subsgps-up-to-conjugacy2 K42 S4)))
-(clojure.pprint/pprint  (let [result (subsgps K42 S4)]
-                          (for [k (sort (keys result))]
-                            [k (count (result k))])))
+;;(time (print-result T3 S3))
+(time (print-result K42 S4))
 
