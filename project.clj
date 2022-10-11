718068d66b058a0b1433fd622431389a329b6411
Attila Egri-Nagy <egri-nagy@Attilas-Mac-mini.local>
2022-10-11 11:53:16 +0900

    updating deps, version bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject kigen "22.05.31"
+(defproject kigen "22.10.10"
   :description "Computational semigroup theory software system shadowing several semigroup packages of the GAP computer algebra system."
   :url "https://github.com/egri-nagy/kigen"
   :license {:name "GNU GENERAL PUBLIC LICENSE"
@@ -7,17 +7,17 @@
                  [org.clojure/math.combinatorics "0.1.6"]
                  [org.clojure/data.int-map "1.0.0"]
                  [orbit "22.04.15"]
-                 [rolling-stones "1.0.2"]
+                 [rolling-stones "1.0.3"]
                  [org.clojure/core.logic "1.0.1"]
                  [com.taoensso/timbre "5.2.1"]
-                 [com.clojure-goes-fast/clj-memory-meter "0.1.3"]
+                 [com.clojure-goes-fast/clj-memory-meter "0.2.1"]
                  [progrock "0.1.2"]
                  [com.bhauman/rebel-readline "0.1.4"]]
   :plugins [[lein-cloverage "1.2.4"]
             [lein-kibit "0.1.8"]
             [lein-ancient "0.7.0"]
             [lein-bikeshed "0.5.2"]
-            [jonase/eastwood "1.2.4"]]
+            [jonase/eastwood "1.3.0"]]
   :jvm-opts ["-Djdk.attach.allowAttachSelf"]
   :main ^:skip-aot kigen.core
   :target-path "target/%s"
