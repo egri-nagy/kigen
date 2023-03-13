f3f76b20189c662748652d1042c4ee0518daa663
Attila Egri-Nagy <attila@egri-nagy.hu>
2023-03-14 08:46:43 +0900

    updated deps, version bump

--- project.clj
+++ project.clj
@@ -1,16 +1,16 @@
-(defproject kigen "22.10.10"
+(defproject kigen "23.03.14"
   :description "Computational semigroup theory software system shadowing several semigroup packages of the GAP computer algebra system."
   :url "https://github.com/egri-nagy/kigen"
   :license {:name "GNU GENERAL PUBLIC LICENSE"
             :url "http://www.gnu.org/copyleft/gpl.html"}
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [org.clojure/math.combinatorics "0.1.6"]
-                 [org.clojure/data.int-map "1.0.0"]
+                 [org.clojure/data.int-map "1.2.0"]
                  [orbit "22.04.15"]
                  [rolling-stones "1.0.3"]
                  [org.clojure/core.logic "1.0.1"]
-                 [com.taoensso/timbre "5.2.1"]
-                 [com.clojure-goes-fast/clj-memory-meter "0.2.1"]
+                 [com.taoensso/timbre "6.1.0"]
+                 [com.clojure-goes-fast/clj-memory-meter "0.2.2"]
                  [progrock "0.1.2"]
                  [com.bhauman/rebel-readline "0.1.4"]]
   :plugins [[lein-cloverage "1.2.4"]
