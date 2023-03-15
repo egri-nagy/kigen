edc56295d0ac2a5b75ae67e5454026801855b5bc
Attila Egri-Nagy <egri-nagy@Attilas-Mac-mini.local>
2023-03-15 12:01:10 +0900

    drafting the transducer constructor

--- experiments/losslessml/lml.clj
+++ experiments/losslessml/lml.clj
@@ -13,7 +13,15 @@
   [A initial-state input-word]
   (reduce
    (fn [state input]
-     ((A state) input))
+     ((A state) input)) ; get the maps for the state, then map the input
    initial-state
    input-word))
 
+(defn construct-transducer
+  "Given the the input-output pairs, and the number of states, this attempts to
+  construct a suitable transducer."
+  [io-pairs n]
+  (let [num-of-output-symbols (count (distinct (map second io-pairs)))]
+    (print n " " num-of-output-symbols)))
+
+(construct-transducer [ [[:a :a ] :q] [ [:b :b] :p]] 4)
