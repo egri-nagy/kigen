88bcfd4f4866dc0620b1d57f2db5846b5502e8c1
Attila Egri-Nagy <attila@egri-nagy.hu>
2023-03-17 17:32:56 +0900

    better ntho with multi-arity

--- experiments/losslessml/lml.clj
+++ experiments/losslessml/lml.clj
@@ -1,29 +1,12 @@
-;; Lossless machine learning: constructing single symbol output transducers
+;; Lossless machine learning: constructing a single symbol output transducer
 ;; from input word, output symbol pairs by logic programming.
+;; In other words, constructing a Moore-machine. https://en.wikipedia.org/wiki/Moore_machine
 (require '[clojure.core.logic :as l])
 (require '[clojure.core.logic.fd :as fd])
 
-;; here is an example of an automaton state transition function
-;; states are nonnegative integers, zero is the initial state
-;; rows are input symbols, so one row is a transformation induced by the input
-(def T [0 0 1
-        1 2 0])
-
-(defn pos
-  "gives the position of the new state in the state-transition table
-   for the current state and input symbol"
-  [num-of-states row column]
-  (+ (* num-of-states row) column))
-
-(defn poso
-  [num-of-states row column result]
-  (l/fresh [prod coord]
-           (fd/* num-of-states row prod)
-           (fd/+ prod column coord)
-           (l/== result coord)))
-
+;; general relational code
 (l/defne reduceo
-  "succeds if the reduction produces the result"
+  "Relational reduce, succeds if the reduction produces the result."
   [relo initial coll result]
   ([_ _ () _] (l/== initial result))
   ([_ _ [next-item . remaining] _]
@@ -31,22 +14,42 @@
             (relo initial next-item result-so-far)
             (reduceo relo result-so-far remaining result))))
 
+(defn ntho
+  ([coll ?n ?x] (ntho coll ?n ?x 0))
+  ([coll ?n ?x current]
+   (l/fresh [head tail]
+            (l/firsto coll head)
+            (l/resto coll tail)
+            (l/conde [(l/== head ?x) (l/== ?n current)]
+                     [(ntho tail ?n ?x (inc current))]))))
+
+;; Internally both the states and the input symbols are represented as nonnegative
+;; integers, for the ease of handling by the logic engine through the finite domain
+;; package.
+;; here is an example of an automaton state transition function
+;; states are nonnegative integers, zero is the initial state
+;; rows are input symbols, so one row is a transformation induced by the input
+(def T [0 0 1 ;states transformed by input symbol 0
+        1 2 0])
+
+(defn pos
+  "It gives the position of the new state in the state-transition table
+   for the current state and input symbol."
+  [num-of-states row column]
+  (+ (* num-of-states row) column))
+
+(defn poso
+  "Relation version of pos. Succeeds if the computed coordinate
+   is the correct one."
+  [num-of-states row column result]
+  (l/fresh [prod coord]
+           (fd/* num-of-states row prod)
+           (fd/+ prod column coord)
+           (l/== result coord)))
+
 (defn state-transition
   [A n state input]
   (nth A (pos n state input)))
-
-(defn NTHO
-  [coll ?n ?x current]
-  (l/fresh [head tail]
-           (l/firsto coll head)
-           (l/resto coll tail)
-           (l/conde [(l/== head ?x) (l/== ?n current)]
-                  [(NTHO tail ?n ?x (inc current))])))
-
-(defn ntho
-  [v ?n ?x]
-  (NTHO v ?n ?x 0))
-
 
 (defn state-transitiono
   [A n state input next-state]
