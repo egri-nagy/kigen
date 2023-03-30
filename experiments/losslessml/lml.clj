1c24f19d57ab6b8adc11f634a4e8bdfeb579b6d0
Attila Egri-Nagy <egri-nagy@Attilas-Mac-mini.local>
2023-03-30 16:04:22 +0900

    adding another manual example

--- experiments/losslessml/lml.clj
+++ experiments/losslessml/lml.clj
@@ -121,3 +121,16 @@
                                :else 2))])
                       (combo/selections [0 1] 4)) 9)))
 
+(def binary
+  [[[0 0 0] :0]
+   [[0 0 1] :1]
+   [[0 1 0] :2]
+   [[0 1 1] :3]
+   [[1 0 0] :4]
+   [[1 0 1] :5]
+   [[1 1 0] :6]
+   [[1 1 1] :7]])
+(display
+ (first (flexible-output-transducer binary 8))
+ (output-fn binary))
+
