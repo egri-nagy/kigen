837fd203780eb3a367df82732100dec5f2d44227
Attila Egri-Nagy <egri-nagy@Attilas-Mac-mini.local>
2023-04-06 15:15:26 +0900

    doing the signal-locators

--- experiments/losslessml/lml.clj
+++ experiments/losslessml/lml.clj
@@ -1,24 +1,46 @@
-(require '[clojure.core.logic :as l])
-(require '[clojure.core.logic.fd :as fd])
 (require '[clojure.math.combinatorics :as combo])
 (require '[kigen.transducer :refer :all])
 (require '[taoensso.timbre :as timbre])
 ;(require '[kigen.transf-conj :as t-c])
 
-
-;;to see trace messages by construct-transducer
-;(timbre/merge-config! {:min-level :trace})
+;; levels: :warn, :info, :debug
 (timbre/set-min-level! :info)
 
-;;signal locators
-(def signal-locator-io
+;;SIGNAL LOCATORS - there is a symbol 1 (the signal) in the sequence of zeroes
+;; where is it?
+;; sl-n-k signal locator for n symbols with k regions 
+;; the number of state is checked to be minimal
+(def sl-3-3
   [[[1 0 0] :first]
    [[0 1 0] :second]
    [[0 0 1] :third]])
-(format-flexible signal-locator-io
-                 (first (flexible-output-transducer signal-locator-io 4)))
+(format-flexible
+ sl-3-3
+ (first (flexible-output-transducer sl-3-3 3)))
 
-(def signal-locator-io2
+(def sl-6-2
+  [[[1 0 0  0 0 0] :first]
+   [[0 1 0 0 0 0] :first]
+   [[0 0 1 0 0 0] :first]
+   [[0 0 0 1 0 0] :second]
+   [[0 0 0 0 1 0] :second]
+   [[0 0 0 0 0 1] :second]])
+(format-flexible
+ sl-6-2
+ (first (flexible-output-transducer sl-6-2 4)))
+
+(def sl-6-3
+  [[[1 0 0  0 0 0] :beginning]
+   [[0 1 0 0 0 0] :beginning]
+   [[0 0 1 0 0 0] :middle]
+   [[0 0 0 1 0 0] :middle]
+   [[0 0 0 0 1 0] :end]
+   [[0 0 0 0 0 1] :end]])
+(format-flexible
+ sl-6-3
+ (first (flexible-output-transducer sl-6-3 4)))
+
+(def sl-6-6
   [[[1 0 0  0 0 0] :1]
    [[0 1 0 0 0 0] :2]
    [[0 0 1 0 0 0] :3]
@@ -26,20 +48,22 @@
    [[0 0 0 0 1 0] :5]
    [[0 0 0 0 0 1] :6]])
 (format-flexible
- signal-locator-io2
- (first (flexible-output-transducer signal-locator-io2 6)))
+ sl-6-6
+ (first (flexible-output-transducer sl-6-6 6)))
 
-(def signal-locator-io3
-  [[[1 0 0  0 0 0  0 0 0] :1]
-   [[0 1 0 0 0 0 0 0 0] :1]
-   [[0 0 1 0 0 0 0 0 0] :1]
-   [[0 0 0 1 0 0 0 0 0] :2]
-   [[0 0 0 0 1 0 0 0 0] :2]
-   [[0 0 0 0 0 1 0 0 0] :2]
-   [[0 0 0 0 0 0 1 0 0] :3]
-   [[0 0 0 0 0 0 0 1 0] :3]
-   [[0 0 0 0 0 0 0 0 1] :3]])
-(first (flexible-output-transducer signal-locator-io3 5))
+(def sl-9-3
+  [[[1 0 0  0 0 0  0 0 0] :1st]
+   [[0 1 0 0 0 0 0 0 0] :1st]
+   [[0 0 1 0 0 0 0 0 0] :1st]
+   [[0 0 0 1 0 0 0 0 0] :2nd]
+   [[0 0 0 0 1 0 0 0 0] :2nd]
+   [[0 0 0 0 0 1 0 0 0] :2nd]
+   [[0 0 0 0 0 0 1 0 0] :3rd]
+   [[0 0 0 0 0 0 0 1 0] :3rd]
+   [[0 0 0 0 0 0 0 0 1] :3rd]])
+(format-flexible
+ sl-9-3
+ (first (flexible-output-transducer sl-9-3 5)))
 
 ;; T has 4 states and 3 input symbols
 (def T [[0 3 1 3] ;states transformed by input symbol 0
