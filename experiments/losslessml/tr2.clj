f753ec3a2be95512f2a5b4a1efbd1e701d99e25f
Attila Egri-Nagy <attila@egri-nagy.hu>
2023-04-16 20:11:45 +0900

    evening commit

--- experiments/losslessml/tr2.clj
+++ experiments/losslessml/tr2.clj
@@ -11,6 +11,11 @@
 (require '[taoensso.timbre :refer [info debug]])
 (require '[kigen.position :refer [index]])
 
+; levels: :warn, :info, :debug
+(timbre/set-min-level! :debug)
+
+(def OGS :_OGS) ;;DO-NOT-USE-THIS-AS INPUT!
+
 ;; this is a good idea, have the map flat (no nesting) so
 ;; the lookup can be done functionally
 (defn geto3
@@ -27,11 +32,12 @@
   (let [states (range num-of-states)
         state-input-pairs (for [q states
                                 i input-symbols]
-                            [q i])]
-    {:delta (zipmap state-input-pairs (repeatedly l/lvar))
-     :omega (repeatedly num-of-outputs l/lvar)}))
+                            [q i])
+        delta (zipmap state-input-pairs (repeatedly l/lvar))]
+    {:delta delta
+     :omega (vec (map delta (for [q states] [q OGS])))}))
 
-(prepare-logic-variables [:a :b] 2 3)
+(prepare-logic-variables [:a :b OGS] 2 3)
 
 (defn trajectory
   "Processes an input word (sequence of input symbols) by an automaton described by the delta state transition function (as vector of vectors) starting from the given initial state.
@@ -39,7 +45,7 @@
   [delta initial-state input-word]
   (reductions
    (fn [state input]
-     ((delta input) state)) ;state transition - more flexible without nth, works for maps too
+     ((delta [state input]))) ;state transition - more flexible without nth, works for maps too
    initial-state
    input-word))
 
@@ -48,7 +54,7 @@
   [delta initial-state input-word]
   (reduce
    (fn [state input]
-     ((delta input) state)) ;not using nth to be more flexible i.e. maps
+     ((delta [state input]))) ;not using nth to be more flexible i.e. maps
    initial-state
    input-word))
 
@@ -58,9 +64,7 @@
   "The relational version of process-word."
   [delta initial-state input-word output]
   (kl/reduceo (fn [state input next-state]
-                (l/fresh [v]
-                         (kl/ntho delta input v)
-                         (kl/ntho v state next-state)))
+                (geto3 delta [state input] next-state))
               initial-state
               input-word
               output))
@@ -95,23 +99,22 @@
   (let [input-symbols (input-symbols-fn io-pairs)
         num-of-inputs (count input-symbols)
         output-symbols (output-symbols-fn io-pairs)
-        output-generator   num-of-inputs ; the extra input symbol to trigger state readout
+        output-generator OGS; the extra input symbol to trigger state readout
         ;;to make the io-pairs work for the fixed engine:
         ;;append an extra symbol for readout and replace the output
         ;;and input symbols with their indices
         modded-io-pairs (for [[input output] io-pairs]
-                          [(vec (concat (map (partial index input-symbols) input)
-                                        [output-generator]))
+                          [(vec (concat  input
+                                         [output-generator]))
                            (index output-symbols output)])
         ;;the finite domains for the search
         outputs (fd/interval 0 (dec (count output-symbols)))
         states (fd/interval 0 (dec n))
         ;;preparing the logic variables: we build a map [state input] -> state
         ;;and a vector for the output mapping
-        lvars  (vec (repeatedly (inc num-of-inputs)
-                                (fn [] (vec (repeatedly n l/lvar)))))
-        state-lvars (apply concat (butlast lvars))
-        output-lvars (last lvars)]
+        result  (prepare-logic-variables (conj input-symbols OGS) (count output-symbols) n)
+        state-lvars (vals (:delta result))
+        output-lvars (:omega result)]
     (info ;bit of information about the processed input
      (+ (count state-lvars) (count output-lvars))
      "logic variables for"
@@ -119,24 +122,26 @@
      num-of-inputs "input symbols"
      (count output-symbols) "output symbols")
     (debug ;debug information about the modified input
-     "modified io pairs" modded-io-pairs
-     input-symbols)
+     "modified io pairs" modded-io-pairs)
+    (debug "shape of result" result)
     (map
      (fn [solution]
-       {:delta (into {} ;formatting a solution into a map with keys :delta :omega (see transducer def.)
-                     (map
-                      (fn [symbol transformation]
-                        [symbol transformation])
-                      input-symbols
-                      (butlast solution)))
-        :omega (mapv (output-symbols-fn io-pairs) (last solution))})
+       ;;just recoding the output
+       (update solution :omega (partial map output-symbols)))
      (l/run* [q]
              (l/everyg #(fd/in % states) state-lvars)
              (l/everyg #(fd/in % outputs) output-lvars)
              (l/everyg (fn [[input output]]
-                         (process-wordo lvars 0 input output))
+                         (process-wordo (:delta result) 0 input output))
                        modded-io-pairs)
-             (l/== q lvars)))))
+             (l/== q result)))))
+
+(def sl-3-3
+  [["|__" :first]
+   ["_|_" :second]
+   ["__|" :third]])
+(def sl-3-3sol (first (transducer sl-3-3 3)))
+(println (trajectories sl-3-3 sl-3-3sol))
 
 (defn check
   "Returns true if the given automaton (defined by solution, state transition function
