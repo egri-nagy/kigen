ee84bd6c1d6a6838e8132017ac2bc7d1bcfc680f
Attila Egri-Nagy <attila@egri-nagy.hu>
2023-04-16 18:10:49 +0900

    refactoring transducer with the more functional lookup (no nesting) - wip

--- experiments/losslessml/tr2.clj
+++ experiments/losslessml/tr2.clj
@@ -1,67 +1,166 @@
 (require '[clojure.math.combinatorics :as combo])
-(require '[kigen.transducer :refer :all])
+;(require '[kigen.transducer :refer :all])
 (require '[taoensso.timbre :as timbre])
 (require '[tangle.core :as tangle])
 (require '[clojure.java.io :refer [copy file]])
 
+(require '[kigen.logic :as kl])
+
 (require '[clojure.core.logic :as l])
 (require '[clojure.core.logic.fd :as fd])
+(require '[taoensso.timbre :refer [info debug]])
+(require '[kigen.position :refer [index]])
 
-(l/defne geto
-  "Succeeds if the map m has key k with value v."
+;; this is a good idea, have the map flat (no nesting) so
+;; the lookup can be done functionally
+(defn geto3
   [m k v]
-  ([() _ v] (l/== v nil))
-  ([[[K . V] . tail] k v]
-   (l/conde [(l/== v V) (l/== k K)]
-            [(l/!= k K) (geto tail k v)])))
+  (l/== v (m k)))
 
-;; this does not work with good-keysvalso TODO: Why?
-;; (defn geto
-;;   [m k v] 
-;;   (l/fresh [K V]
-;;            (l/== k K)
-;;            (l/== v V)
-;;            (l/membero [K V] m)))
+(let [result {:hey {[:x 0] (l/lvar) [1 "hey"] (l/lvar)}}]
+  (l/run 1 [q] (l/== q result)
+         (geto3 (:hey result) [1 "hey"] :b)))
 
-(l/defne good-keysvalso
-  [m kdom vdom]
-  ([() _ _] l/succeed)
-  ([[[K . V] . tail] kdom vdom]
-   (fd/in K kdom)
-   (fd/in V vdom)
-   (good-keysvalso tail kdom vdom)))
+(defn prepare-logic-variables
+  "This gives the shape of the solution."
+  [input-symbols num-of-outputs num-of-states]
+  (let [states (range num-of-states)
+        state-input-pairs (for [q states
+                                i input-symbols]
+                            [q i])]
+    {:delta (zipmap state-input-pairs (repeatedly l/lvar))
+     :omega (repeatedly num-of-outputs l/lvar)}))
 
-(defn nonmembero2
-  [x l]
-  (l/fresh [h t]
-           (l/conde
-            [(l/== l ())]
-            [(l/conso h t l)
-             (l/!= x h)
-             (nonmembero2 x t)])))
+(prepare-logic-variables [:a :b] 2 3)
 
-(l/defne good-mapo
-  [m banned]
-  ([() _] l/succeed)
-  ([[[k . _] . tail] banned]
-   (l/nafc l/membero k banned)
-   ;(nonmembero2 k banned)
-   (l/fresh [newbanned]
-            (l/appendo k banned newbanned)
-            (good-mapo tail newbanned))))
+(defn trajectory
+  "Processes an input word (sequence of input symbols) by an automaton described by the delta state transition function (as vector of vectors) starting from the given initial state.
+   The whole trajectory (initital, all intermittent states and final state) is returned."
+  [delta initial-state input-word]
+  (reductions
+   (fn [state input]
+     ((delta input) state)) ;state transition - more flexible without nth, works for maps too
+   initial-state
+   input-word))
 
-(let [kdom (fd/domain 1 2)
-      vdom (fd/domain 3 4)]
-  (l/run 3 [q]
-         (geto q 1 4)
-         (good-keysvalso q kdom vdom)
-         (good-mapo q [])
-         ))
+(defn process-word
+  "Processes an input word (sequence of input symbols) by an automaton described by the delta state transition function (as vector of vectors) starting from the given initial state. It returns the resulting state."
+  [delta initial-state input-word]
+  (reduce
+   (fn [state input]
+     ((delta input) state)) ;not using nth to be more flexible i.e. maps
+   initial-state
+   input-word))
 
-(def interval (vec (range 1 4)))
-(l/run* [a b c]
-      (l/membero a interval)
-      (l/membero b interval)
-      (l/membero c interval)
-      (l/project [a b c]
-               (l/== 2 (count (set (list a b c))))))
+;; relational code is after the functional one to see the connection
+;; we have to use ntho explicitly (only works vectors internally)
+(defn process-wordo
+  "The relational version of process-word."
+  [delta initial-state input-word output]
+  (kl/reduceo (fn [state input next-state]
+                (l/fresh [v]
+                         (kl/ntho delta input v)
+                         (kl/ntho v state next-state)))
+              initial-state
+              input-word
+              output))
+
+;; FLEXIBLE INPUT OUTPUT TRANSDUCER CONSTRUCTION ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; input words can be sequences of any type of distinct entities
+;; similarly, outputs can be of any types
+;; internal states are nonnegtaive integers
+
+(defn output-symbols-fn
+  "Returns all collected output symbols appearing in the input-output
+   pairs without repetition. Returned as a vector, the indices can be used
+   to refer to the symbols. The order of the symbols defined by the order
+   of their appeareance in the io-pairs (through distinct)."
+  [io-pairs]
+  (vec (distinct (map second io-pairs))))
+
+(defn input-symbols-fn
+  "Returns all collected input symbols appearing in the input-output
+   pairs without repetition. Returned as a vector, the indices can be used
+   to refer to the symbols. The order of the symbols defined by the order
+   of their appeareance in the io-pairs (through distinct)."
+  [io-pairs]
+  (vec (distinct (mapcat first io-pairs))))
+
+(defn transducer
+  "Given the input-output pairs, and the number of states, this attempts to
+  construct a suitable transducer.
+   It produces all solutions lazily, so if only a solution is needed, first
+   can be used."
+  [io-pairs n]
+  (let [input-symbols (input-symbols-fn io-pairs)
+        num-of-inputs (count input-symbols)
+        output-symbols (output-symbols-fn io-pairs)
+        output-generator   num-of-inputs ; the extra input symbol to trigger state readout
+        ;;to make the io-pairs work for the fixed engine:
+        ;;append an extra symbol for readout and replace the output
+        ;;and input symbols with their indices
+        modded-io-pairs (for [[input output] io-pairs]
+                          [(vec (concat (map (partial index input-symbols) input)
+                                        [output-generator]))
+                           (index output-symbols output)])
+        ;;the finite domains for the search
+        outputs (fd/interval 0 (dec (count output-symbols)))
+        states (fd/interval 0 (dec n))
+        ;;preparing the logic variables: we build a map [state input] -> state
+        ;;and a vector for the output mapping
+        lvars  (vec (repeatedly (inc num-of-inputs)
+                                (fn [] (vec (repeatedly n l/lvar)))))
+        state-lvars (apply concat (butlast lvars))
+        output-lvars (last lvars)]
+    (info ;bit of information about the processed input
+     (+ (count state-lvars) (count output-lvars))
+     "logic variables for"
+     n "states"
+     num-of-inputs "input symbols"
+     (count output-symbols) "output symbols")
+    (debug ;debug information about the modified input
+     "modified io pairs" modded-io-pairs
+     input-symbols)
+    (map
+     (fn [solution]
+       {:delta (into {} ;formatting a solution into a map with keys :delta :omega (see transducer def.)
+                     (map
+                      (fn [symbol transformation]
+                        [symbol transformation])
+                      input-symbols
+                      (butlast solution)))
+        :omega (mapv (output-symbols-fn io-pairs) (last solution))})
+     (l/run* [q]
+             (l/everyg #(fd/in % states) state-lvars)
+             (l/everyg #(fd/in % outputs) output-lvars)
+             (l/everyg (fn [[input output]]
+                         (process-wordo lvars 0 input output))
+                       modded-io-pairs)
+             (l/== q lvars)))))
+
+(defn check
+  "Returns true if the given automaton (defined by solution, state transition function
+   and output function) will indeed produce the output values given in the io-pairs.
+   It uses format-flexible for processing the raw solution."
+  [io-pairs {delta :delta omega :omega}]
+  (every? (fn [[input output]]
+            (= output (omega (process-word delta 0 input))))
+          io-pairs))
+
+(defn trajectories
+  "Creates string representations of all trajectories by the io-pairs."
+  [io-pairs {delta :delta omega :omega}]
+  (map ;we are going through all input-out pairs
+   (fn [[input output]] ;representing one trajectory in a string 
+     (let [trj (trajectory delta 0 input)
+           final (omega (last trj))]
+       (apply str (concat (map (fn [q i] (str q " "
+                                              ;"(" (omega q) ") "
+                                              "·"  i " "))
+                               trj
+                               input)
+                          [(last trj) " = " final
+                           (if (= output final)
+                             " ✔"
+                             " ✘")]))))
+   io-pairs))
