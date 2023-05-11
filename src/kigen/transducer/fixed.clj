de63753a866da6f3f3e5f51b82ba09607301bcfc
egri-nagy <attila@egri-nagy.hu>
2023-05-11 18:06:35 +0900

    comments in fixed

--- src/kigen/transducer/fixed.clj
+++ src/kigen/transducer/fixed.clj
@@ -1,33 +1,32 @@
 (ns kigen.transducer.fixed
-  " FIXED OUTPUT TRANSDUCER CONSTRUCTION ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-this is the first working version of transducer synthesis so it is somewhat of
+  "FIXED OUTPUT TRANSDUCER CONSTRUCTION
+This is the first working version of transducer synthesis so it is somewhat of
 a legacy code. The input-output pairs need to be given in the internal representation
 (nonnegative integers) thus the output of the Moore automaton is hardcoded.
-There is also the issue of using the initial state 0 as an output which may be a too
+Consequently, there is the issue of using the initial state 0 as an output which may be a too
 rigid constraint."
   (:require
    [clojure.core.logic :as l]
    [clojure.core.logic.fd :as fd]
-   [taoensso.timbre :refer [info debug]] 
+   [taoensso.timbre :refer [info debug]]
    [kigen.transducer.common :refer [process-wordo input-symbols-fn]]))
 
 (defn fixed-output-transducer
   "Given the the input-output pairs, and the number of states, this attempts to
   construct a suitable transducer. Both the inputs and the outputs are represented as
-  nonnegative integers. The output is delta, the first is the transformation realized by
-   input symbol 0, second for input symbol 1, and so on.
-  It produces all solutions lazily, so if only a solution is needed, first
-  can be used."
+  nonnegative integers. The output delta is a vector of transformations.
+  The first vector is the transformation realized by
+  input symbol 0, second for input symbol 1, and so on.
+  It produces all solutions lazily, so if only a solution is needed, the first
+  one can be used."
   [io-pairs n]
-  (let [input-symbols (input-symbols-fn io-pairs)
+  (let [i (count (input-symbols-fn io-pairs))
         statesfd (fd/interval 0 (dec n))
-        delta (vec
-               (repeatedly (count input-symbols)
-                           (fn [] (vec (repeatedly n l/lvar)))))
+        delta (vec (repeatedly i (fn [] (vec (repeatedly n l/lvar)))))
         lvars (apply concat delta)]
     (info (count lvars) "logic variables for"
           n "states"
-          (count input-symbols) "symbols")
+          i "symbols")
     (debug lvars)
     (map (fn [solution]
            {:delta solution
