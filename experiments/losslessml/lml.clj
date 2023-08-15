5bee0c66a88c2c6195334278468e59ba869460b3
Attila Egri-Nagy <attila@egri-nagy.hu>
2023-08-15 15:10:56 -0400

    separating the PDF-generation from dotting

--- experiments/losslessml/lml.clj
+++ experiments/losslessml/lml.clj
@@ -142,8 +142,8 @@
 (trajectories binary binarysol)
 (check binary binarysol)
 
-(defn DotSolution2PDF
-  [io-pairs {omega :omega delta :delta} name]
+(defn DotSolution
+  [io-pairs {omega :omega delta :delta}]
   (let [nodes (map
                (fn [state]
                  {:id (str "node" state)
@@ -156,16 +156,20 @@
                     [(str "node" a) (str "node" b) {:label input-sym}])
                   (range) (delta input-sym)))
                (input-symbols-fn io-pairs))]
-    (copy (tangle/dot->image (tangle/graph->dot
-                              nodes
-                              edges
-                              {:directed? true ;:node {:shape :box}
-                               :node->id (fn [n] (if (keyword? n) (name n) (:id n)))
-                               :node->descriptor (fn [n] (when-not (keyword? n) n))})
-                             "pdf")
-          (file (str name ".pdf")))))
+    (tangle/graph->dot
+     nodes
+     edges
+     {:directed? true ;:node {:shape :box}
+      :node->id (fn [n] (if (keyword? n) (name n) (:id n)))
+      :node->descriptor (fn [n] (when-not (keyword? n) n))})))
 
-(DotSolution2PDF sl-3-3 sl-3-3sol "sl-3-3")
+(defn DotSolution2PDF
+  [dotstring name] 
+  (copy (tangle/dot->image dotstring
+                           "pdf")
+        (file (str name ".pdf"))))
+
+(DotSolution2PDF (DotSolution sl-3-3 sl-3-3sol) "sl-3-3")
 (DotSolution2PDF zo zosol "zo")
 (DotSolution2PDF binary binarysol "binary")
 
