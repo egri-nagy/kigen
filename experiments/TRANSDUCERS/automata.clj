42cccac9dc661c9692b4cbec5fcc9d40b2613a8e
Attila Egri-Nagy <attila@egri-nagy.hu>
2023-09-20 08:24:10 +0900

    starting to work on the recognizer

--- experiments/TRANSDUCERS/automata.clj
+++ experiments/TRANSDUCERS/automata.clj
@@ -4,6 +4,8 @@
 ;;TODO write a function that checks the io-pairs for contradicting pairs
 ;; like the same word requiring two different outputs
 
+(def stop \⏹)
+
 (defn proper-prefixes
   "All proper prefixes of the given word, starting from the empty word."
   [coll]
@@ -20,3 +22,26 @@
   [io-pairs]
   (update-vals (group-by second io-pairs)
                (partial map first)))
+
+(defn firsts-in-trie
+  "Returns a set of the elements that appear the first positions in the
+   branches."
+  [trie]
+  (cond
+    (empty? trie) #{}
+    (vector? (first trie)) (into #{} (map first (first trie)))
+    :else #{(first trie)}))
+
+(defn recognizer
+  "Returns a recognizer FA for the trie."
+  ([trie] (recognizer trie 0 {} #{:accept}))
+  ([trie state delta acceptors]
+   (let [elts (take-while (fn [x] (not (vector? x))) trie)
+         (reduce (fn [])
+                 delta)]
+     (if (= (count trie) (count elts))
+       (conj result new-so-far)
+       (reduce
+        (fn [r v]  (retrieve v new-so-far r))
+        result
+        (last trie))))))
