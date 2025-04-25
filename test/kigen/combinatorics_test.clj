2f9b55575450f49011203f7d00f694ee8e5fbafb
Attila Egri-Nagy <attila@egri-nagy.hu>
2025-04-25 17:41:48 +0900

    adding new test

---
+++ test/kigen/combinatorics_test.clj
@@ -0,0 +1,14 @@
+(ns kigen.combinatorics-test
+  (:require [clojure.test :refer :all]
+            [kigen.combinatorics :refer :all]))
+
+(deftest big-enough-partitions-test
+  (testing "big-enough-partitions"
+    (is (=  #{'(#{0 1} #{2} #{3})
+              '(#{0 2} #{1} #{3})
+              '(#{0} #{1 2} #{3})
+              '(#{0 3} #{1} #{2})
+              '(#{0} #{1 3} #{2})
+              '(#{0} #{1} #{3 2})
+              '(#{0} #{1} #{2} #{3})}
+            (set (big-enough-partitions (range 4) 3))))))
