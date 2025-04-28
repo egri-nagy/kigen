a8dcc1bfd8f226aa7b2deff1418aafb4f512ed6c
egri-nagy <attila@egri-nagy.hu>
2025-04-28 15:15:33 +0900

    just thinking about the implementation of transformation semigroupoids

---
+++ src/kigen/semigroupoid/transformation.clj
@@ -0,0 +1,14 @@
+(ns kigen.semigroupoid.core
+  "The core definitions for dealing with semigroupoids.")
+
+; an example semigroupoid element
+{:src 1
+ :trg 4
+ :t [0 0 1 1]
+}
+
+; example transformation semigroupoid
+{:objects [1,2,4,2]
+ :gens [{:src 1, :trg 2, :t [1]}]}
+
+
