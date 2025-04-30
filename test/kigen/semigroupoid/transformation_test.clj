9727cad0d438b68bf2b44bec90e9c94f4b6ffa88
Attila Egri-Nagy <attila@egri-nagy.hu>
2025-04-30 18:12:12 +0900

    adding test

--- test/kigen/semigroupoid/transformation_test.clj
+++ test/kigen/semigroupoid/transformation_test.clj
@@ -13,3 +13,17 @@
 (deftest dual-mode-counter-semigroupoid-from-generators
   (testing "Testing the closure algorithm for semigroupoid generators."
     (is (= 15 (count (sgpoid-by-gens dual-mode-counter-gens))))))
+
+; Example 3.2 - original example missing arrow ef
+(def two-objs-seven-arrows
+  [{:s 0 :t 0 :m [1 0]} ;a
+   ;{:s 0 :t 0 :m [0 1]} ;b
+   {:s 0 :t 1 :m [0 1]} ;c
+   ;{:s 0 :t 1 :m [1 0]} ;d
+   {:s 0 :t 1 :m [0 0]} ;e
+   {:s 1 :t 1 :m [1 1]} ;f
+   ])
+
+(deftest Ex3.2-semigroupoid-from-generators
+  (testing "Example 3.2"
+    (is (= 7 (count (sgpoid-by-gens two-objs-seven-arrows))))))
