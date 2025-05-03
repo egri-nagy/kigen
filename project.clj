06472f335f989fbae43bcbdc4b130beccbc81e0b
Attila Egri-Nagy <attila@egri-nagy.hu>
2025-05-03 13:08:06 +0900

    better to have longer name

--- project.clj
+++ project.clj
@@ -21,7 +21,7 @@
             [lein-ancient "0.7.0"]
             [lein-bikeshed "0.5.2"]
             [jonase/eastwood "1.4.3"]
-            [cider/cider-nrepl "0.55.3"]]
+            [cider/cider-nrepl "0.55.7"]]
   :jvm-opts ["-Djdk.attach.allowAttachSelf"]
   :main ^:skip-aot kigen.core
   :target-path "target/%s"
