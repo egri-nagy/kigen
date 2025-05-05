c4c3480aba3088400a4cca1b9419379771be216e
Attila Egri-Nagy <attila@egri-nagy.hu>
2025-05-05 19:11:20 +0900

    using  only composable pairs - so we can work with nth instead of get

--- src/kigen/semigroupoid/endomorphism.clj
+++ src/kigen/semigroupoid/endomorphism.clj
@@ -7,33 +7,39 @@
 (defn compf
   "Composition function for the given composition table S." 
   [S a b]
-  (get (get S a) b)) ;get is more tolerant than nth, gives nil when troubled
+  (nth (nth S a) b)) ;get is more tolerant than nth, gives nil when troubled
+
+(defn composable-pairs
+  "All the composable pairs of elements of semigroupoid S given as a composition
+   table."
+  [S]
+  (let [n (count S)]
+    (for [a (range n)
+          b (range n)
+          :when (compf S a b)]
+      [a b])))
 
 (defn endomorphic?
   "S is a composition table of a semigroupoid
    phi is a vector representing the endomorphism
    just for checking how to do it functionally, before realtionally"
   [S phi]
-  (let [n (count S)
-        pairs (for [a (range n) b (range n)] [a b])]
-    (every? (fn [[a b]]
-              (let [ab (compf S a b)]
-                (or (nil? ab) ; noncomposable arrow can go to composable one!
-                    (= (get phi (compf S a b))
-                       (compf S (get phi a) (get phi b))))))
-            pairs)))
+  (every? (fn [[a b]]
+            (= (phi (compf S a b))
+               (compf S (phi a) (phi b))))
+          (composable-pairs S)))
 
 (defn endomorphisms
   "Logic search for all endomoprhisms of semigroupoid S given as a composition
    table."
   [S] ;given as a composition table
   (let [n (count S)
-        lvars (repeatedly n l/lvar)
+        phi (repeatedly n l/lvar)
         elts (fd/interval 0 (dec n))]
     (l/run*
      [q]
-     (l/everyg #(fd/in % elts) lvars)
-     (l/== q lvars))))
+     (l/everyg #(fd/in % elts) phi)
+     (l/== q phi))))
 
 (def S
   [[0 1 2 3 4 nil]
@@ -47,4 +53,4 @@
 (count (filter (partial endomorphic? S)
                (map vec (endomorphisms S))))
 
-(endomorphic? S (vec (repeat 6 1)))
+(endomorphic? S (vec (repeat 6 0)))
