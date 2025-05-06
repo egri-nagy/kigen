571ab1bf44b839aa96593d034baae94fc3510a28
Attila Egri-Nagy <attila@egri-nagy.hu>
2025-05-06 21:05:53 +0900

    idea is working, but there was some trouble with the nils

--- src/kigen/semigroupoid/endomorphism.clj
+++ src/kigen/semigroupoid/endomorphism.clj
@@ -6,7 +6,7 @@
             [kigen.logic :refer [ntho]]))
 
 (defn compf
-  "Composition function for the given composition table S." 
+  "Composition function for the given composition table S."
   [S a b]
   (nth (nth S a) b)) ;get is more tolerant than nth, gives nil when troubled
 
@@ -16,7 +16,6 @@
    [v]
    (ntho S a v)
    (ntho v b ab)))
-
 
 (defn composable-pairs
   "All the composable pairs of elements of semigroupoid S given as a composition
@@ -29,18 +28,31 @@
       [a b])))
 
 (defn endomorphic?
+  "ab - an element of S
+   pairs - all the a,b pairs such that ab=ab"
+  [S phi ab pairs]
+  (every?
+   (fn [[a b]] (= (phi ab) (compf S (phi a) (phi b))))
+   pairs))
+
+(defn endomorphico
+  [S ab pairs]
+  ;(println "ab:" ab)
+  ;(println "pairs:" pairs)
+  (l/everyg (fn [[a b]]
+              (compfo S a b ab))
+            pairs))
+
+(defn endomorphism?
   "another test for endomorphisms, trying to reformulate constrains"
   [S phi]
-  (let [s2factors (group-by (fn [[a b]] (compf S a b))
-                            (composable-pairs S))]
-    (every? (fn [s] (every?
-                     (fn [[a b]]
-                       (= (phi s) (compf S (phi a) (phi b))))
-                     (s2factors s)))
-           (keys s2factors))))
+  (let [ab2factors (group-by (fn [[a b]] (compf S a b))
+                             (composable-pairs S))]
+    (every? (fn [[ab pairs]]
+              (endomorphic? S phi ab pairs))
+            ab2factors)))
 
-
-(defn endomorphic2?
+(defn endomorphism2?
   "S is a composition table of a semigroupoid
    phi is a vector representing the endomorphism
    just for checking how to do it functionally, before realtionally"
@@ -55,12 +67,23 @@
    table."
   [S] ;given as a composition table
   (let [n (count S)
-        phi (repeatedly n l/lvar)
+        phi (vec (repeatedly n l/lvar))
         elts (fd/interval 0 (dec n))
-        pairs (composable-pairs S)]
+        ab2factors
+        (-> (group-by (fn [[a b]] (compf S a b))
+                      (composable-pairs S))
+            (update-keys phi)
+            (update-vals (partial map (partial map phi))))
+        S2 (mapv (partial mapv #({nil n} % %)) S)]
+    ;(println "phi:" phi)
+    ;(println "ab2factors" ab2factors) (println)
     (l/run*
      [q]
      (l/everyg #(fd/in % elts) phi)
+     (l/everyg (fn [[ab pairs]]
+                 ;(println ab pairs)
+                 (endomorphico S2 ab pairs))
+               ab2factors)
      (l/== q phi))))
 
 (def S
@@ -71,8 +94,20 @@
    [nil nil nil nil nil 2]
    [nil nil nil nil nil 5]])
 
+(count (endomorphisms S))
+
+(group-by (fn [[a b]] (compf S a b))
+          (composable-pairs S))
+
+;what pairs produce 3?
+(l/run*
+ [p q]
+ (compfo S p q 3))
+
+(count (endomorphisms S))
+
 ;quick to get the endomorphisms
-(count (filter (partial endomorphic? S)
+(count (filter (partial endomorphism? S)
                (map vec (endomorphisms S))))
 
-(endomorphic? S (vec (repeat 6 0)))
+(endomorphism? S (vec (repeat 6 0)))
