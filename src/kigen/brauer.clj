883ea323d035dcb121576bc2cc09ec29fcdcb56a
Attila Egri-Nagy <attila@egri-nagy.hu>
2025-06-08 10:41:12 +0900

    standard generators implemented

--- src/kigen/brauer.clj
+++ src/kigen/brauer.clj
@@ -11,11 +11,16 @@
   (interleave (repeat x) (repeat y)))
 
 (defn trace
+  "Trace a path starting from the given point. This is used when stacking two
+   diagrams for composition. The second argument, diagrams, should contain a
+   lazy infinite list, alternating the diagrams. When composing a and b, tracing
+   a point from a should have the list a,b,a,b,a,b,...; while tracing from b
+   should have b,a,b,a,..."
   [start diagrams]
   (loop [point start
          processed #{start}
          diags diagrams]
-    (let [npt ((first diags) point)]
+    (let [npt ((first diags) point)] ; we follow an edge here 
       (if (or (nil? npt)
               (processed npt))
         [point processed]
@@ -25,11 +30,15 @@
   "Given half the maps individually (arrows in one direction), this completes
    the diagram."
   [maps]
-  (mapv second
-        (sort (into maps
-                    (map (fn [[s t]] [t s]) maps)))))
+  (mapv second ; the second coord becomes the image
+        (sort ;by the first coordinate
+         (into maps ;combine the existing maps with their reverses
+               (map
+                (fn [[s t]] [t s]) ;the opposite direction
+                maps)))))
 
 (defn mul
+  "Composing two Brauer diagrams, assumed to be same degree."
   [a b]
   (let [n (/ (count a) 2) ; get the degree
         nnils (repeat n nil) ; for padding 
@@ -43,19 +52,40 @@
                (if (empty? pts)
                  pairs
                  (let [pt (first pts)]
-                   (if (processed pt)
+                   (if (processed pt) ; it's already traced, go to next point
                      (recur pairs
                             processed
                             (rest pts))
-                     (let [diags (if (< pt n)
+                     (let [diags (if (< pt n) ; are we tracing from top?
                                    (alternate A B)
                                    (alternate B A))
                            [img traced] (trace pt diags)]
                        (recur (conj pairs [(shift-back pt)
                                            (shift-back img)])
                               (into processed traced)
-                              (rest pts)))))))] 
+                              (rest pts)))))))]
     (complete maps)))
+
+(defn brauer-gens
+  "The standard generators for the symmetric group and a cup-cap."
+  [n]
+  (let [identity (when (= n 1)
+                   (complete [[0 1]]))
+        transposition (when (> n 1)
+                        (into [[0 (inc n)] [1 n]]
+                              (map (fn [x] [x (+ x n)])
+                                   (range 2 n))))
+        cycle (when (> n 2)
+                (into [[(dec n) n]]
+                      (map (fn [x] [x (inc (+ x n))])
+                           (range (dec n)))))
+        cup-cap (when (> n 1)
+                  (into [ [0 1] [n (inc n)]]
+                        (map (fn [x] [x (+ x n)])
+                             (range 2 n))))]
+    (map complete
+         (remove nil? [identity transposition cycle cup-cap]))))
+
 
 (def i [3 4 5 0 1 2]) ;identity
 (def t [4 3 5 1 0 2]) ;transposition
