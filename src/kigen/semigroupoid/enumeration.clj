022ff9766f463df81fb1eb6a9a3fa156aeb851f7
egri-nagy <attila@egri-nagy.hu>
2025-06-11 14:40:25 +0900

    it checks for order 2 semigroupoids

--- src/kigen/semigroupoid/enumeration.clj
+++ src/kigen/semigroupoid/enumeration.clj
@@ -41,26 +41,37 @@
      (l/everyg (partial associativo S) triples)
      (l/== q S))))
 
+(defn composablo
+  "The goal for associativity for a given triple."
+  [S a b elts]
+  (l/fresh
+   [ab]
+   (compfo S a b ab)
+   (fd/in ab elts)))
+
 (defn semigroupoids-order-n
   "Enumerating semigroups of order n by constructing all n by n composition
    tables.
-   Constraints: table entries should be in 0..n-1 or nil,
-   and all triples should satisy associativity."
+   Constraints: table entries should be in 0..n-1, all triples should satisy
+   associativity."
   [n]
   (let [elts (fd/interval 0 (dec n))
+        allvals (fd/interval 0 n) ;we include n itself, instead of nil
         S (vec (repeatedly n (fn [] (vec (repeatedly n l/lvar)))))
         lvars (apply concat S)
         triples (selections (range n) 3)]
     (l/run*
      [q]
-     (l/everyg #(l/conde  ;nil or element
-                 [(l/nilo %)]
-                 [(fd/in % elts)])
-               lvars)
+     (l/everyg #(fd/in % allvals) lvars)
      (l/everyg (fn [[a b c]]
-                 (l/conda
-                  [(compfo S a b nil)]
-                  [(compfo S b c nil)]
-                  [(associativo S [a b c]) ]))
-               triples)
+                 (l/conde
+                  [(composablo S a b elts) ;both composable and associative
+                   (composablo S b c elts)
+                   (associativo S [a b c])]
+                  [(composablo S a b elts) ;a,b composable; b,c not
+                   (compfo S b c n)]
+                  [(composablo S b c elts) ;b,c composable, a,b not
+                   (compfo S a b n)]
+                  [(compfo S a b n) ; neither composable
+                   (compfo S b c n)])) triples)
      (l/== q S))))
