d6c2b0e37359ab95b9c33f347e055805d3e5ed27
Attila Egri-Nagy <attila@egri-nagy.hu>
2025-06-27 17:35:43 +0900

    just experiments

--- experiments/SEMIGROUPOIDS/enumerate.clj
+++ experiments/SEMIGROUPOIDS/enumerate.clj
@@ -2,11 +2,16 @@
 ;; v25.06.xx
 (require '[kigen.semigroupoid.enumeration :refer [semigroupoids-order-n]])
 
-(def S '[0 - -
-         - 0 -
-         - - 0])	
+(def S '[3 - -
+         - 3 -
+         - - 3])	
 
-(doseq [sgpoid (semigroupoids-order-n 3 S)]
+(def S2 '[4 - - -
+          - 4 - -
+          - - 4 -
+          - - - 4])	
+
+(doseq [sgpoid (semigroupoids-order-n 4 S2)]
   (println sgpoid))
 
 ;(println (count (semigroupoids-order-n 3)))
