4f523b7cf3c775c18ad22272b5957e703d421441
Attila Egri-Nagy <attila@egri-nagy.hu>
2025-06-27 20:50:51 +0900

    more system information + commented out

--- src/kigen/core.clj
+++ src/kigen/core.clj
@@ -19,7 +19,11 @@
   "The first argument is a name of a file containing Clojure source code.
   This main method evaluates the forms contained."
   [& args]
-  (println "KIGEN v" (get-version)
-           " Computational Semigroup Theory Software System")
+  (println "; KIGEN" (get-version)
+           "Clojure" (clojure-version)
+           "Java" (System/getProperty "java.vm.version")
+           (System/getProperty "os.name")
+           (System/getProperty "os.version")
+           (System/getProperty "os.arch"))
   (load-file (first args))
   (shutdown-agents))
