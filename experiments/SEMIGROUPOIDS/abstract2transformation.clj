14a157dcd3ca8b809136e4d5e4e893915c095bd5
Attila Egri-Nagy <attila@egri-nagy.hu>
2025-07-02 17:00:10 +0900

    adding comments

--- experiments/SEMIGROUPOIDS/abstract2transformation.clj
+++ experiments/SEMIGROUPOIDS/abstract2transformation.clj
@@ -1,17 +1,17 @@
 ;; finding transformation representation of an abstract semigroupoid
-;; v25.06.xx
+;; v25.07.02
 (require '[kigen.semigroupoid.homomorphism :refer [isomorphisms]])
 (require '[kigen.semigroupoid.transformation :refer [comptab
                                                      full-semigroupoid]])
 
 ;; Example 3.2 from Representation Independent Decompositions of Computation https://arxiv.org/abs/2504.04660
 (def S
-  [[0 1 2 3 4 nil]
-   [1 0 2 4 3 nil]
-   [nil nil nil nil nil 2]
-   [nil nil nil nil nil 2]
-   [nil nil nil nil nil 2]
-   [nil nil nil nil nil 5]])
+  [[0 1 2 3 4 :n]
+   [1 0 2 4 3 :n]
+   [:n :n :n :n :n 2]
+   [:n :n :n :n :n 2]
+   [:n :n :n :n :n 2]
+   [:n :n :n :n :n 5]])
 
 ;; two objects with two states fully connected
 (def T (comptab (full-semigroupoid [[0 0] [0 1] [1 1] [1 0]] [2 2])))
@@ -25,6 +25,7 @@
 (def T3 (comptab (full-semigroupoid [[0 0] [1 1]] [2 2])))
 (count (isomorphisms S T3))
 
+;; with the strict moprhism, it is not expected to have embeddings here
 (def T4 (comptab (full-semigroupoid [[0 0]] [3])))
 (count (isomorphisms S T4))
 
