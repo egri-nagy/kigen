0a1b7b4283df50ab6f5c45fb04326ae862589017
Attila Egri-Nagy <attila@egri-nagy.hu>
2025-07-22 20:11:11 +0900

    revising morphic.clj

--- src/kigen/morphic.clj
+++ src/kigen/morphic.clj
@@ -1,25 +1,23 @@
 (ns kigen.morphic
-  "The most abstract description of checking whether a relation is morphic
-   or not.
-   morph-m  - morphisms are represented as hash-maps, thus they can be partial ")
+  "The most abstract description morphic relations and functions.
+   morph-m  - called 'morphism map', morphisms are represented as hash-maps,
+   thus they can be partially defined")
 
 (defn morphic?
   "Decides whether `morph-m` is a morphic relation for elements `a` and `b`.
+  More descriptive names would be potentially-morphic?, or morphic-so-far?, as
+  we need a defined incompatibility to refute, partial defaults to morphic.
   `source-composition-fn` binary composition operation in the source domain
   `target-composition-fn` same in the target domain
   `eq-fn` equality test in the target domain (= for homomorphisms,
   subset? for relational morphisms)
   This abstract function is not meant to be used directly, but for creating
-  custom tests by pre-filling the arguments by partial.
-  If the product ab in the source domain does not yet have a morphic image,
-  then there is nothing to check, so it returns true, meaning that the
-  we have a partial morphism, which potentially can be completed."
+  custom tests by pre-filling the arguments by partial."
   [eq-fn
    source-composition-fn
    target-composition-fn
    morph-m
-   a
-   b]
+   a b]
   (let [mab (morph-m (source-composition-fn a b))]
     (or
      (nil? mab) ;if morph-m is partial then we consider it potentially morphic
