393d7e3ef33eade388b7c0b2352a8debd21b998a
Attila Egri-Nagy <attila@egri-nagy.hu>
2025-08-05 17:25:33 +0900

    revising done

--- experiments/DIAGSGPS/playground.clj
+++ experiments/DIAGSGPS/playground.clj
@@ -53,55 +53,65 @@
  [2 2 1 0 1 2])
 
 ;;;;; New canonical labeling algorithms ;;;;;;;;;;;;;;;;;;;;;;;
-(defn canonical-labeling
-  "Canonical labeling of a sequence `t`. The elements of the sequence get
+(defn can-lab-seq
+  "Canonical labeling of a sequence `s`. The elements of the sequence get
    converted to non-negative integers in ascending order. A hash-map is
    constructed to map elements to integers bijectively.
    `PL`, a data structure for representing a partial labeling can be given in
    the form [mappings next-available-integer]. It can be used for canonical
-   labeling of several sequences."
-  ([t] (canonical-labeling [{} 0] t))
-  ([PL t] ; canonical labeling data can be given
+   labeling of several sequences. This is also the ouput."
+  ([s] (can-lab-seq [{} 0] s))
+  ([PL s] ; canonical labeling data can be given
    (reduce
     (fn [[mappings available] element]
       (if (contains? mappings element)
         [mappings available]
         [(conj mappings [element available]) (inc available)]))
     PL
-    t)))
+    s)))
 
-(defn seq-canonical-labeling
-  "Canonical labeling of a sequence of sequences."
-  [tseq]
-  (let [[p _] (reduce canonical-labeling
-                      (canonical-labeling (first tseq))
-                      (rest tseq))]
-    [(mapv (partial mapv p) tseq)
-     p]))
+(defn can-seq
+  [s]
+  (let [[m _] (can-lab-seq s)]
+    (mapv m s)))
 
+(defn can-lab-seq-seq
+  "Canonical labeling of a sequence of sequences. It simply threads partial
+   labelings through the sequence by calling [[can-lab-seq]].
+   It returns the labeling."
+  [sseq]
+  (let [[m _] (reduce can-lab-seq
+                      (can-lab-seq (first sseq))
+                      (rest sseq))]
+    m))
 
-(defn boo
-  "processing one partially created conjrep
-   `remaining` - the set of unprocessed transformations"
-  [[processed remaining CL]]
-  (let [ts2CL (zipmap remaining
-                      (map (partial canonical-labeling CL) remaining))
-        tts2ts (group-by (fn [t] (mapv (first (ts2CL t)) t)) remaining)
-        minrep (first (sort compare (keys tts2ts)))]
-    (zipmap (tts2ts minrep) (map  ts2CL (tts2ts minrep)))
-    (for [t (tts2ts minrep)]
-      [(conj processed minrep)
-       (remove #{t} remaining)
-       (ts2CL t)])))
+(defn can-seq-seq
+  "The canonical representation of a sequence of sequences."
+  [sseq]
+  (mapv (partial mapv (can-lab-seq-seq sseq)) sseq))
 
-(boo [[] ts [{} 0]])
+(defn can-lab-set-seq
+  "Canonical labeling of a set of sequences."
+  [sset]
+  (let [boo (fn [[done waiting PL]]
+              (let [s2PL (zipmap waiting
+                                 (map (partial can-lab-seq PL) waiting))
+                    canon-form (fn [s] (mapv (first (s2PL s)) s))
+                    cans2s (group-by canon-form waiting)
+                    minrep (first (sort compare (keys cans2s)))]
+                (for [t (cans2s minrep)]
+                  [(conj done minrep)
+                   (remove #{t} waiting)
+                   (s2PL t)])))]
+    (loop [labelings [[[] sset [{} 0]]]] ;starting with the empty labeling
+      (if (empty? (second (first labelings))) ;i.e., done is empty
+        (map (fn [[rep _ p]] [rep p]) labelings) ;the output
+        (let [nlabelings (mapcat boo labelings)
+              latest2labs (group-by (comp peek first) ;only the last can differ
+                                    nlabelings)
+              minrep (first (sort compare (keys latest2labs)))]
+          (recur (latest2labs minrep)))))))
 
-(defn set-canonical-labeling
-  [tset]
-  (loop [labelings [[[] tset [{} 0]]]]
-    (if (empty? (second (first labelings)))
-      (map (fn [[rep _ p]] [rep p]) labelings)
-      (let [nlabelings (mapcat boo labelings)
-            latest2labs (group-by (comp peek first) nlabelings)
-            minrep (first (sort compare (keys latest2labs)))]
-        (recur (latest2labs minrep))))))
+(defn can-set-seq
+  [sset]
+  (first (first (can-lab-set-seq sset))))
